d7c9b962c4308466bff673a8d799179394aba9d5
nanitight <ntpnaane@gmail.com>
2021-05-31 19:59:59 +0200

    Initialize the react front-end

---
+++ frontend/graph-path/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        Graph Path
+        </header>
+        <div className="GraphDashboard">
+          Dashboard
+        </div>
+    </div>
+  );
+}
+
+export default App;
