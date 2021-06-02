fa7ecd6d14b6712992c3b647f225bd15c7ecd53f
Kage-001 <73745316+Kage-001@users.noreply.github.com>
2021-06-02 23:30:49 +0200

    Updated routing to handle post requests

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express();
 
 const ProjectRoute = require('./api/routes/Project')
-
+app.use(express.urlencoded({extended:true}));
 
 app.use('/project' , ProjectRoute);
 module.exports = app
