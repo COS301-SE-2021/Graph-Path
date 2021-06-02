6e6b06675860925d438af3d977771364a64f865d
Kage-001 <73745316+Kage-001@users.noreply.github.com>
2021-06-02 23:32:05 +0200

    Updated the CreateNewProject function to woth with MongoDB

--- Services/CreateNewProject.js
+++ Services/CreateNewProject.js
@@ -4,48 +4,41 @@ const mongoose = require('mongoose');
 const ProjectModel = require('../Models/ProjectModel');
 
 
-
+DB_URI = "mongodb+srv://NoCap2021:NoCap2021@cluster0.n67tx.mongodb.net/GraphPath?retryWrites=true&w=majority";
 function createNewProject(MetaData)
 {
 
     mongoose.connect(DB_URI, {useNewUrlParser: true , useUnifiedTopology: true})
         .then((result) =>{
 
-            console.log("connected");
+            const project = new ProjectModel ({
+                _id: mongoose.Types.ObjectId(),
+                name: MetaData['Name'],
+                id: MetaData['id'],
+                startDate: MetaData['startDate'],
+                dueDate: MetaData['dueDate'],
+                Members: MetaData['Members'],
+                Owner: MetaData['Owner'],
+                Graph: MetaData['Graph']
+            });
+
+            project.save().then(result =>{
+
+
+                return 1;
+
+            })
+                .catch(err=>{
+                    console.log("error saving document: "+ err);
+
+                    return 0;
+                })
         })
+
+
         .catch((err) => {
 
         })
-
-     console.log(MetaData);
-
-    const project = new ProjectModel ({
-
-        _id: mongoose.Types.ObjectId,
-        name: MetaData['Name'],
-        id: MetaData['id'],
-        startDate: MetaData['startDate'],
-        dueDate: MetaData['dueDate'],
-        Members: MetaData['Members'],
-        Owner: MetaData['Owner'],
-        Graph: MetaData['Graph']
-    });
-
-    project.save().then(result =>{
-
-        console.log(result);
-        status = 1;
-        return status;
-
-    })
-        .catch(err=>{
-            console.log("error saving document: "+ err);
-            status = 0;
-            return 0;
-        })
-
-
-
 
 }
 
