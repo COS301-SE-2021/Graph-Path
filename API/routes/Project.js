6de03ad09ca5547f30a1145387195d52aa8ae35a
Kage-001 <73745316+Kage-001@users.noreply.github.com>
2021-06-02 22:32:59 +0200

    Modified changed route for creating new Projects

--- API/routes/Project.js
+++ API/routes/Project.js
@@ -5,26 +5,36 @@ const retriveProject = require('../../Services/RetrieveProjects');
 
 const router = express.Router();
 
-router.get('/:projectName' , (req,res,next) =>{
+router.post('/newProject' , (req,res,next) =>{
+    data  = req.body
+    status = createNewProject(data);
+    if(status ==1)
+    {
+        res.status(200).json({
 
-    const projectName = req.params.projectName;
-    res.status(200).json({
+            statusCode : 1,
+            message : "new project creation successful",
+            error : "",
+            body: ""
 
-        Name: projectName,
-        id: "000",
-        startDate: "2021-01-01",
-        dueDate:"2021-05-05",
-        Members:{
-            developer1 : "Person 1",
-            developer2 : "Person 2",
-            developer3 : "Person 3",
-        },
-        Owner: "Bob Vans",
-        Graph:" some object" ,
-    })
+        })
+    }
+
+    else
+    {
+        res.status(200).json({
+
+            statusCode : 0,
+            message : "Projection creation failed",
+            error : "",
+            body: ""
+        })
+
+    }
+
+
 
 })
-
 router.get('/:projectId' , (req,res,next) =>{
 
     res.status(200).json({
