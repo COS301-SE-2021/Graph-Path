acb9010067b534aaabf89e6d5e88b5346414f9ff
nanitight <ntpnaane@gmail.com>
2021-06-02 05:09:21 +0200

    added cors

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
-const express = require('express')
-const app = express();
+//npm module require
+const express = require('express') ;
+const cors = require('cors') ;
 
+//added files require
 const ProjectRoute = require('./api/routes/Project')
 
+//Initialize APP
+const app = express();
 
+//Middleware
+app.use(cors()) ; 
+app.use(express.json()) ; 
+app.use(express.urlencoded({extended:true})) ; 
+
+//Routes
 app.use('/project' , ProjectRoute);
+
+//Module Export
 module.exports = app;
