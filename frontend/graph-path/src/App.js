b8afa50ef7c1acb991c9ec1006161241d1cf37da
nanitight <ntpnaane@gmail.com>
2021-06-03 12:14:02 +0200

    Fixed React import statemnt

--- frontend/graph-path/src/App.js
+++ frontend/graph-path/src/App.js
@@ -1,8 +1,6 @@
 import React from 'react';
-import Header from './components/Header' ; 
 import './css/App.css';
-import React, { Component}from 'react' ;
-/*import React, { Component} from 'react'*/
+import Header from './components/Header' ; 
 import Dashboard from './components/Dashboard' ;
 import Register from './components/Register';
 import Login from './components/Login'
