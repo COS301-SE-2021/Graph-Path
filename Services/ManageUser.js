d329c7bdd357e154422762c68014667177173db7
Aliandro <u19016477@tuks.co.za>
2021-06-03 13:23:55 +0200

    Created the ManagerUser.js file

---
+++ Services/ManageUser.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+const Usermodel = require('../Models/UserModel');
+
+function getAllUsers(body){
+
+}
+
+function manageUser(body) {
+    //connect to DB
+    //create user object for DB{model}
+    const user = new UserModel({
+        _id: mongoose.Types.ObjectId(),
+        name: body.name,
+        Surname: body.Surname,
+        email:body.email,
+        password: body.password,
+        username: body.username,
+        type: body.type,
+        Notification: body.Notification
+    });
+    //push/save user object in DB
+
+    //if(push successfull return true){
+
+    //}else return false
+    return 0;
+
+
+}
+
+var userManagement = {};
+userManagement.getAllUsers = getAllUsers;
+userManagement.insertNewUser = manageUser;
+module.exports = userManagement;
