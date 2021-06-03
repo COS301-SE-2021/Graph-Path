66f7ba69a7554c7c23889f317fc3436fdf0d95ae
Aliandro <u19016477@tuks.co.za>
2021-06-03 13:22:01 +0200

    Added the user routes

--- API/routes/User.js
+++ API/routes/User.js
@@ -1,2 +1,40 @@
 const express = require('express')
 const router = express.Router();
+const userManagement = require('../../Services/ManageUser')
+
+router.get('/userlist',(req, res, next)=> {
+
+    body = req.body;
+    res.status(200).json({
+        body
+    })
+    //
+});
+
+router.post('/insertUser',(req, res, next)=> {
+
+    var body= req.body;
+    if( userManagement.insertNewUser(body) == 0){
+        res.status(200).json({
+
+            status: true,
+            message: "The user was inserted successfully."
+
+        })
+    }else{
+        res.status(200).json({
+
+            status: false,
+            message: "The user was not inserted successfully."
+
+        })
+    }
+    //Pass body into function :   boolean insertNewUser(body)
+    //if (insertNewUSer == true){
+    //send response (JSON object) ={status:0, message: "user was inserted"}
+    //}else{
+    //send response (JSON object) ={status:1, message: "error"}
+    //}
+});
+
+module.exports = router;
