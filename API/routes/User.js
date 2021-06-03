335f9f1fc96d33d4ca304b67f457e7713d4ada0c
Kage-001 <73745316+Kage-001@users.noreply.github.com>
2021-06-03 11:40:01 +0200

    Added to User Routing

--- API/routes/User.js
+++ API/routes/User.js
@@ -30,19 +30,38 @@ router.get('/userlist',(req, res, next)=> {
 
 });
 
-router.post('/insertUser',(req, res, next)=>{
-    var user= {
+router.post('/insertUser',(req, res, next)=> {
+    var user = {
         Name: "john",
         Surname: "doe",
         id: 1,
-        email:"John@gmail.com"
+        email: "John@gmail.com"
     }
-    mongo.connect(url,(err, db)=>{
-        db.collection('Users').insertOne(user, (err,result)=>{
+    mongo.connect(url, (err, db) => {
+        db.collection('Users').insertOne(user, (err, result) => {
             console.log("We inserted the user into the database: " + user);
             db.close();
         });
     });
+
+    //*******************************************************************
+    //Step 1:  Take request Body :  body = req.body
+    /*Step 2:  body should look like this body= { name:
+                                            Surname:
+                                            email
+                                            password
+                                            username
+                                            type:
+                                            Notification:
+                                            */
+    //step 3: res.status(200).json({body })
+
+    // step 4: use postman to check if it returns the body as expected
+
+    // from here i'll help you setup User into a mongoDB
+
+
+
 
 });
 
