1c97ab02288198bba45cf7ec2e146b32fc41f4f7
Aliandro <u19016477@tuks.co.za>
2021-06-03 13:18:06 +0200

    Added the user routes

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ const express = require('express')
 const app = express();
 
 const ProjectRoute = require('./api/routes/Project')
+const UserRoute= require('./API/routes/User');
 app.use(express.urlencoded({extended:true}));
 
 app.use('/project' , ProjectRoute);
+app.use('/user', UserRoute);
 module.exports = app
