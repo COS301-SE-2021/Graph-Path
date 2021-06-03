adcb10dd9d29af05ebc7e0dbedc0977dab3d80e3
Aliandro <u19016477@tuks.co.za>
2021-06-03 06:31:30 +0200

    Added get projectlist and insertproject function

--- API/routes/Project.js
+++ API/routes/Project.js
@@ -1,9 +1,13 @@
 const express = require('express')
 const router = express.Router();
-const mongo = require('mongodb');
+const mongo = require('mongodb').MongoClient;
 const assert = require('assert');
 
 var url = 'mongodb+srv://NoCap2021:NoCap2021@cluster0.n67tx.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+//var url = 'mongodb+srv://Aliandro:Aliandro2000@cluster0.y5ggo.mongodb.net/Graph-Path?\n' +
+    //'retryWrites=true&w=majority';
+
+
 
 router.get('/:projectName' , (req,res,next) =>{
 
@@ -44,24 +48,26 @@ router.get('/:projectId' , (req,res,next) =>{
 
 })
 
-router.get('/users',(req, res, next)=> {
-    res.status(200).json({
-        "Name": "John",
-        "surname": "",
+router.post('/insertProject',(req, res, next)=>{
+    var project ={
 
-    })
-});
-
-router.post('/insertUser',(req, res, next)=>{
-    var user= {
-        Name: "john",
-        Surname: "doe",
-        id: 1,
-        email:"John@gmail.com"
     }
     mongo.connect(url,(err, db)=>{
-        db.collection('Users').insertOne(user, (err,result)=>{
-            console.log("We inserted the user into the database: " + user);
+        db.collection('Projects').insertOne(project, (err,result)=>{
+            console.log("We inserted the project into the database: " + project);
+            db.close();
+        });
+    });
+});
+
+router.get('/projectlist',(req, res, next)=> {
+
+    var arr =[];
+    mongo.connect(url,(err,db)=>{
+        var cursor = db.collection('Projects').find();
+        cursor.forEach((proj,err)=>{
+            arr.push(proj);
+        },()=>{
             db.close();
         });
     });
