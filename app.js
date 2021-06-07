4cbc6dea10431bccfd42cba6abc164e4609eccf9
nanitight <ntpnaane@gmail.com>
2021-06-07 08:58:00 +0200

    Added cors

--- app.js
+++ app.js
@@ -1,10 +1,35 @@
 const express = require('express')
 const app = express();
-
+const cors = require('cors') ;
 const ProjectRoute = require('./api/routes/Project')
 const UserRoute= require('./API/routes/User');
+app.use(cors()) ;
 app.use(express.urlencoded({extended:true}));
 
 app.use('/project' , ProjectRoute);
 app.use('/user', UserRoute);
+
+
+//Handling Errors?
+app.use((req,res,next)=>{
+    const error = new Error('Not found') ;
+    error.status = 404 ;
+    next(error) ;
+    // console.log('Created Error') ;
+    // console.log('req body:',JSON.stringify(req.body) ) ;
+    // console.log('req files?',JSON.stringify(req.files) ) ;
+}) ;
+
+app.use((error,req,res,next)=>{
+    res.status(error.status || 500) ;
+    res.json({
+        error:error.message,
+        type:error.type ,
+        loc: error.prototype,
+        stack:error.stack,
+        url:req.url
+
+    });
+}) ;
+
 module.exports = app
