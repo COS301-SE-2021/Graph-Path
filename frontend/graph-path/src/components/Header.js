c8ddb16e372bc76f353048138bc559c12a0ce204
nanitight <ntpnaane@gmail.com>
2021-06-07 19:36:16 +0200

    Added Links and switch for interaction

--- frontend/graph-path/src/components/Header.js
+++ frontend/graph-path/src/components/Header.js
@@ -1,30 +1,65 @@
 import React from 'react' ;
+import Dashboard from './Dashboard' ;
+import Register from './Register';
+import Login from './Login' ;
+import '../css/App.css';
+import {BrowserRouter as Router, Switch,Route,Link} from 'react-router-dom' ;
+
 
 class Header extends React.Component{
-    // constructor(props){
-    //     super(props) ; 
-    //     this.state = {
-    //         log:false
-    //     }
-    // } 
+    constructor(props){
+        super(props) ; 
+        this.state = {
+            log:false
+        }
+    } 
+
     render(){
-        if (this.props.log){
+        if (this.state.log){
             return (
                 <header className="App-header">
                 <div>Graph Path</div>    
                 <div>
                     <form >
-                        <input type="submit" value="LogOff" onClick={this.props.to} />  
-                    </form></div>
+                        <input className="App-link"
+                         type="submit" value="LogOff" 
+                         onClick={this.props.logOut} />  
+                    </form>
+                    </div>
                 </header>
             )
 
         }
-        return (
-    <header className="App-header">
-            Graph Path
-            </header>
-        )
+        else{
+            return (
+                <Router>
+                <div>
+                    <header className="App-header">
+                    Graph Path
+                    </header>
+                    {/* <form className="form-header"> */}
+                        <Link className="App-link" to="/signIn" >LogIn</Link>
+                        {/* <input className="App-link"
+                         type="submit" value="LogIn" 
+                         onClick={this.props.logOut} />  
+                         */}
+                        <Link className="App-link" to="signUp">SignUp</Link>
+                        {/* <input className="App-link"
+                         type="submit" value="SignUp" 
+                         onClick={this.props.logOut} />  */}
+                    {/* </form> */}
+                    <Switch>
+                        <Route path="/signIn">
+                            <Login logIn={this.props.LogIn} />
+                        </Route>
+                        <Route path="/signUp">
+                            <Register />
+                        </Route>
+                    </Switch>
+                </div>
+                </Router>
+            )
+        }
     }
 }
 
