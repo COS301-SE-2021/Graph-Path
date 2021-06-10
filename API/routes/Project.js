292c13509240de9114128f735f154b224cbe801a
nanitight <ntpnaane@gmail.com>
2021-06-10 15:26:44 +0200

    New db connection service

--- API/routes/Project.js
+++ API/routes/Project.js
@@ -2,17 +2,79 @@ const express = require('express');
 // const createNewProject = require('../../Services/CreateNewProject');
 //swap out service
 const projectManager = require('../../Services/ProjectService');
+const mongoose = require('mongoose') ;
 const router = express.Router();
-
+var db = require('../../Controllers/DBController').getDB() ;
 // var url = 'mongodb+srv://<p>:NoCap2021@cluster0.n67tx.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
 //var url = 'mongodb+srv://<p>:Aliandro2000@cluster0.y5ggo.mongodb.net/Graph-Path?\n' +
     //'retryWrites=true&w=majority';
 
 router.post('/newProject' ,  (req,res,next) =>{
-    console.log('received request ',req,'servicing.....') ;
+    if (req == undefined || req.body == undefined ){
+        res.json({
+            message:"Req is null" 
+        }) ;
+    }
+    if (req.body.projectName == undefined){
+        console.log('no projec name')
+        res.send({
+            message:"Please specify a Project Name"
+        }) ;
 
+    }
+    console.log('received request ',req.body,'servicing.....') ;
+    var data = req.body ;
+    const id = new mongoose.mongo.ObjectID() ;
+    data["_id"] = id ;
+    db.collection('Projects').insertOne(data) 
+    .then((ans)=>{
+        console.log('success',ans) ;
+        res.send(ans.ops) ;
+    },(ans)=>{
+        console.log('rejected',ans) ; 
+        res.send({
+            data:ans
+        }) ;
+    }) 
+    .catch(err=>{
+        console.log('from db req',err)
+    })
 }) ; 
 
+router.get('/find',(req,res,next)=>{
+    
+    db.collection('Projects').findOne({})
+    .then((ans)=>{
+        console.log('success',ans) ;
+        res.send({
+            data:ans
+        }) ;
+    },(ans)=>{
+        console.log('rejected',ans) ; 
+        res.send({
+            data:ans
+        }) ;
+    }) 
+    .catch(err=>{
+        console.log('from db req',err)
+    })
+}) ;
+
+router.get('/list',(req,res,next)=>{
+    console.log('received request ',req.body,'servicing.....') ;
+    const projects = db.collection('Projects').find({})
+    console.log('success',ans) ;
+    if (projects){
+        res.send({
+            message:projects//.json()
+        }) ;
+    }
+    else{
+        res.send({
+            message:"Error"
+        })
+    }
+})
 
 router.post('/createnewProject' ,  (req,res,next) =>{
     // try{
