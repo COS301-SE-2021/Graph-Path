4d047faef83343677d1ffd5eed83a2cdf50d93f3
Unabashed-Thabo <82472351+Unabashed-Thabo@users.noreply.github.com>
2021-06-14 09:54:09 +0200

    Update Register.js

--- frontend/graph-path/src/components/Register.js
+++ frontend/graph-path/src/components/Register.js
@@ -1,23 +1,27 @@
 import React from 'react'
 import '../css/Register.css'
 import axios from 'axios';
-import {useState,useEffect} from 'react'
+
 
 const emailRegex = RegExp(/^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/)
+/*Rest part checks for submitting null values for all inputs*/
+const formValid = ({formErrors,...rest})=>{
+    let valid =true;
 
-const useForm = () => {
-    const [values,setvalues]=useState({
-        firstName: '',
-        lastName:'',
-        userName:'',
-        email:'',
-        password:''
-    })
+    //Checks for individual null values
+    Object.values(formErrors).foreach(val=> {
+            val.length> 0 && (valid=false);
+        }
+    );
+
+    //Checks for when you submit form with all null values
+    Object.values(rest).forEach(val=> {
+        val==null && (valid=false);
+    });
+
+    return valid;
 }
-
-
 class Register extends React.Component{
-    //const [errors, setErrors]= useState({})
     state ={
         firstName:'',
         lastName:'',
@@ -28,64 +32,84 @@ class Register extends React.Component{
         api:'http://localhost:9001',
         answer:null,
 
-
-
-
-
+        //Errors
+        formErrors: {
+            firstName:"",
+            lastName:"",
+            userName:"",
+            email:"",
+            password:""
+        }
     }
 
-
     change =(e) => {
-        this.setState({
-            [e.target.name]: e.target.value
-        });
+        //this.setState({
+          //  [e.target.name]: e.target.value});
+
+        e.preventDefault();
+
+        const {name,value}=e.target;
+
+        let formErrors = {...this.state.formErrors};
+
+        switch(name){
+            case 'firstName':
+                formErrors.firstName = value==null  ? 'FirstName is required'
+                    : "";
+                break;
+
+            case 'lastName':
+                formErrors.lastName = value==null ? 'LastName is required'
+                    : "";
+                break;
+
+            case 'userName':
+                formErrors.userName = value==null ? 'userName is required'
+                    : "";
+                break;
+
+            case "email":
+                formErrors.email = emailRegex.test(value)
+                    ? ""
+                    : "Invalid email address";
+                break;
+
+            case 'password':
+                formErrors.password = value.length < 9 ? 'Minimum for password should be 8 characters'
+                    : "";
+                break;
+
+            default: break;
+        }
+
+        this.setState({ formErrors, [name]: value });
+
     }
 
     onSubmit =e=>{
         e.preventDefault(); /*So the values entered don't show on URL and NOT to submit by itself*/
         // this.props.onSubmit(this.state);
         //display loading screen
-        this.setState({
-            wait:true
-        }) ;
-
-        console.log(this.state)
-        const data = {
-            firstName:this.state.firstName ,
-            surname:this.state.lastName,
-            email:this.state.email,
-            username:this.state.userName,
-            password:this.state.password,
-            type: "Standard User",
-            Notification:"Email"
-        }
-
-        console.log(data)
-
-        //talk to api
-        // fetch('http://localhost:9001/user/newUser',{
-        //     method:'POST',
-        //     body:data
-        // }) 
-        // .then(res => res.json())
-        // .then(res =>{
-        //     console.log(res) ;
-        //     alert('user register, please try login')
-        // })
-        // .catch(err =>{
-        //     if (err){
-        //         console.log(err) ;
-        //     }
-        // })
-        //
-        // this.setState({
-        //     wait:true
-        // })
-        this.sendData(data) ;
 
 
-        /*...*/
+            this.setState({
+                wait:true
+            }) ;
 
+            console.log(this.state)
+            const data = {
+                firstName:this.state.firstName ,
+                surname:this.state.lastName,
+                email:this.state.email,
+                username:this.state.userName,
+                password:this.state.password,
+                type: "Standard User",
+                Notification:"Email"
+            }
+
+            console.log(data)
+
+             this.sendData(data) ;
 
         /*Clear inputs once you click on submit*/
         this.setState({
@@ -97,46 +121,51 @@ class Register extends React.Component{
             wait:false
         });
     }
-    
+
+
     sendData (data){
         //path to make the post and wait for the response
-    axios.post(`${this.state.api}/user/newUser`,data) 
-    .then((response) =>{
-       if(response.status===400){
-           throw Error(response.statusText) ;
-       }//else
-       console.log('from back end',response)
+        axios.post(`${this.state.api}/user/newUser`,data)
+            .then((response) =>{
+                if(response.status===400){
+                    throw Error(response.statusText) ;
+                }//else
+                console.log('from back end',response)
 
-       const res = response.data;
-       console.log(res) ;
-       this.setState({
-           answer:res.message,
-           responseData:res.data //data
-        },()=>{
-        //    alert('res:'+this.state.answer)
-           console.log(this.state)
-           if (this.state.answer!== null && this.state.answer){
-            //    this.props.changeToDefault() ;
-           }
-        }) 
-    
-    },(response)=>{
-        console.log('rejected',response) ;
-    })
-    .catch((error)=>{
-        console.log(error) ;
-    })
-}
+                const res = response.data;
+                console.log(res) ;
+                this.setState({
+                    answer:res.message,
+                    responseData:res.data //data
+                },()=>{
+
+                    console.log(this.state)
+                    if (this.state.answer!== null && this.state.answer){
+                        //    this.props.changeToDefault() ;
+                    }
+                })
+
+            },(response)=>{
+                console.log('rejected',response) ;
+            })
+            .catch((error)=>{
+                console.log(error) ;
+            })
+    }
+
+
+
+
     render(){
 
-
+        const {formErrors} = this.state;
 
         if (this.state.wait){
             document.getElementById("registerscreen").className.push('wait')
         }
         else{
-            var scrn = document.getElementById('registerscreen') ; 
-            if (scrn !== null) scrn.className ="" ; 
+            var scrn = document.getElementById('registerscreen') ;
+            if (scrn !== null) scrn.className ="" ;
         }
         var ans = this.state.answer ;
         if (ans != null || ans != undefined){
@@ -149,47 +178,61 @@ class Register extends React.Component{
                 <form className="logForm" onSubmit={this.onSubmit} >
                     <p>First Name</p>
                     <input
+                        className={formErrors.firstName.length > 0 ? 'error': null}
                         name='firstName'
-                           type='text'
+                        type='text'
                         placeholder='First Name' value={this.state.firstName}
-                            onChange={e=>this.change(e)}
+                        onChange={this.change}
                     />
-
+                    {formErrors.firstName.length > 0 && (
+                        <span className='errorMessage'>{formErrors.firstName}</span>
+                    )}
                     <p>Last Name</p>
                     <input
+                        className={formErrors.lastName.length > 0 ? 'error': null}
                         name='lastName' type='text'
                         placeholder='Last Name' value={this.state.lastName}
-                        onChange={e=>this.change(e)}
+                        onChange={this.change}
                     />
-
+                    {formErrors.lastName.length > 0 && (
+                        <span className='errorMessage'>{formErrors.lasttName}</span>
+                    )}
                     <p>Username</p>
                     <input
+                        className={formErrors.userName.length > 0 ? 'error': null}
                         name= 'userName' type='text'
                         placeholder='Username' value={this.state.userName}
-                        onChange={e=>this.change(e)}
+                        onChange={this.change}
                     />
-
+                    {formErrors.userName.length > 0 && (
+                        <span className='errorMessage'>{formErrors.userName}</span>
+                    )}
                     <p>Email</p>
                     <input
+                        className={formErrors.email.length > 0 ? 'error': null}
                         name = 'email'
                         type='email'
                         placeholder='Email' value={this.state.email}
-                        onChange={e=>this.change(e)}
+                        onChange={this.change}
                     />
+                       {formErrors.email.length > 0 && (
+                           <span className='errorMessage'>{formErrors.email}</span>
+                        )}
 
                     <p>Password</p>
                     <input
+                        className={formErrors.password.length > 0 ? 'error': null}
                         name='password'
                         type='password'
                         placeholder='Password' value={this.state.password}
                         onChange={e=>this.change(e)}
                     />
-
-                <br />
-                        <button  className="btn1" type="submit"> Submit </button>
-                        <small>
-                            Already Have an Account? Login <a href="#">Here</a>
-                        </small>
+                    {formErrors.password.length > 0 && (
+                        <span className='errorMessage'>{formErrors.password}</span>
+                    )}
+                    <br />
+                    <button  className="btn1" type="submit"> Submit </button>
+                    <small>Already Have an Account? Login <a href="#">Here</a></small>
                 </form>
             </div>
         );
