62a1ae78bac640285a1621af320588a782ffea5e
Kage-001 <73745316+Kage-001@users.noreply.github.com>
2021-06-17 16:45:18 +0200

    Updated the Task Manager service

---
+++ Services/TaskManger.js
@@ -0,0 +1,14 @@
+
+
+MockTask = {
+    CreationDate: Date.now() ,
+    DueDate: Date.now()+1 ,
+    TaskName: "Task 0 ",
+    Description: "This is a test task",
+    Label: "Green",
+    Status: 'In progress',
+    Assignee: ['User1' , 'User2'],
+    Assigner: "Kagiso 1",
+    Parent_Node : "SomeID of the parent Node",
+
+}
