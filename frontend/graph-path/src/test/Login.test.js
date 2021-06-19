fe2dc4f739706f449f7022596ee85491d39ccf35
Livhuwani Mvhali <u17262578@tuks.co.za>
2021-06-19 13:39:25 +0200

    Updated Login Test

--- frontend/graph-path/src/test/Login.test.js
+++ frontend/graph-path/src/test/Login.test.js
@@ -13,4 +13,16 @@ describe("Login Test", () =>{
     test("wraps the login form with a div",()=>{
         expect(wrapper.find(".loginScreen").length).toEqual(1);
     });
-})
+
+    test("there is a form",()=>{
+        expect(wrapper.find("form")).toBeTruthy();
+    });
+
+    test("render title",()=>{
+        expect(wrapper.find("h4").text()).toBe("Sign In");
+    });
+
+    test("render two input field and button of input element", ()=>{
+        expect(wrapper.find("input").length).toEqual(3);
+    });
+});
