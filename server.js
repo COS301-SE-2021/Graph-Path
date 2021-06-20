6e11f4cbc6a58a536bc711ed3d11c8380ad5e704
Kage-001 <73745316+Kage-001@users.noreply.github.com>
2021-06-20 10:01:20 +0200

    Adjusted server setup for Jest setup

--- server.js
+++ server.js
@@ -1,9 +1,35 @@
 const http = require('http');
-const app = require('./app');
-const port = process.env.PORT || 9001 || 3000 ; 
-
-const server = http.createServer(app);
-
+const port = process.env.PORT || 9001 || 3000 ;
+const mongoDBInstance = require('./Controllers/DBController')
+//const server = http.createServer(app);
+/*
 server.listen(port,()=>{
     console.log(`server running on http://localhost:${port}`)
 }) ;
+
+
+server.on("listening", () => {
+    const addr = server.address();
+    console.log("This my adr: ",addr)
+    const bind = typeof addr === "string" ? `pipe ${addr}` : `port ${addr.port}`;
+    // eslint-disable-next-line no-console
+    console.log(`Listening on ${bind}`);
+})
+*/
+
+mongoDBInstance.connect( (error) => {
+
+    if(error){
+        console.log('error message here', error);
+    }
+
+    const app = require('./app');
+    const server = http.createServer(app);
+
+    server.listen(port,()=>{
+        console.log(`server running on http://localhost:${port}`)
+    }) ;
+
+})
+
+
