4cf9f7fd6db882e58d268638bdd5d5d29535da36
nanitight <ntpnaane@gmail.com>
2021-07-30 15:11:38 +0200

    Add Node and Edge Manager in one

--- 1.Website/frontend/src/components/Node.js
+++ 1.Website/frontend/src/components/Node.js
@@ -1,28 +1,132 @@
-import React from 'react'
+import React from 'react' ; 
+import Task from './Task' ;
+import EdgeManager from './Edge';
+import {Link,withRouter, Route, Switch} from 'react-router-dom' ;
+// import '../css/Dashboard.css' ;
+
+class GraphManager{
+    constructor(graph){
+      if (graph === undefined || graph.nodes === undefined || graph.edges === undefined){
+        this.setGraph("") ;
+      }
+      else{
+        this.graph = graph ;
+      }
+    }
+  
+    setGraph(graph){
+      if (Array.isArray(graph.nodes) && Array.isArray(graph.edges) ){
+        this.graph = graph ;
+      }
+      else{
+        this.graph = {
+            nodes : [],
+            edges : []
+        }
+      }
+    }
+    getGraph(){
+      return this.graph ;
+    }
+    showNodeList(){
+      var nodes = this.graph.nodes ; 
+      if(nodes.length <= 0){
+        console.log('Nothing in the Array') ; 
+      }
+      else{
+        console.log(`${nodes.length} Nodes`)
+      }
+    }
+    addEdge=(src,tgt)=>{
+        var edg ;
+        var curr = this.graph ; 
+        edg = {id:`e${curr.edges.length+1}`, // give edge an id
+            source:src, 
+            target:tgt,
+            label:`${src} to ${tgt}` ,
+            color:'#00ff00'
+        }
+        curr.edges.push(edg) ;
+       
+    }
+    
+    addNode = (fromTask) =>{
+        // add the node and give it an id
+        var curr = this.graph ; 
+        var obj = {
+            label:fromTask.taskName , // give it lable fromTask
+            size:300,
+        }; 
+        // if there was already a node?
+        if (curr.nodes.length>0){
+            obj["id"]= `n${curr.nodes.length+1}` ;
+            obj["color"] = '#0000ff' ;
+            curr.nodes.push(obj) ;
+        }
+        else{
+            // add node with edge depending on self
+            obj["id"]= `n1` ;
+            obj["color"] = '#ff0000' ; //start is red
+            curr.nodes.push(obj) ;
+        }
+    }
+}
 
 class Node extends React.Component{
-    // constructor(props){
-    //     super(props) ; 
-    //     this.state ={
-    //         task:this.props.task ,
-    //         started: false
-    //     }
-    // }
+    constructor(props){
+        super(props) ; 
+        this.state ={
+        }
+        this.graphManager = null  ; 
+    }
+
+    componentDidMount(){
+        this.graphManager = new GraphManager(this.props.graph)
+        if (this.props.graph !== undefined){
+            this.graphManager.setGraph(this.props.graph)
+        }
+    }
+    addNewNode = (name)=>{
+        this.graphManager.addNode(name) ; 
+        this.updateParent(this.graphManager.getGraph()) ;
+    }
+    addNewEdge =(param1,param2)=>{
+        this.graphManager.addEdge(param1,param2) ;
+        this.updateParent(this.graphManager.getGraph()) ;
+    }
+    updateParent=(graph)=>{
+        if (typeof this.props.updateGraph === 'function'){
+            this.props.updateGraph(graph) ;
+        }else{
+            alert('Could not update graph') ;
+        }
+    }
 
     render(){
-        var color = "red"
-        if (this.props.color){
-            color = this.props.color ; 
-            console.log("color: ",color) ; 
-        }
-
+        const {match} = this.props ;
+    
         return (
-            <a >
-        <svg width="120">
-            <circle cx="60" cy="80" r="50" fill={color} onClick={this.displayTask}/>
-            {/* <div>{this.state.task}</div> */}
-        </svg>
-        </a>
+            <div>
+                <div>
+                    <Link to={`${match.url}/addNode`}>Add Nodes</Link><span> </span>
+                    <Link to={`${match.url}/edges`}>Add Edges</Link>
+                    <br/>
+                </div>
+                <Switch>
+                    <Route path={`${match.url}/addNode`} >
+                        
+                        <Task addTask={this.addNewNode} 
+                        />
+                    </Route>
+                    <Route path={`${match.url}/edges`}>
+                        <EdgeManager graphToDisplay={this.props.graph}
+                            addEdge={this.addNewEdge}
+                        />
+                    </Route>
+                </Switch>
+            
+        </div>
+        
         )
     }
 
@@ -31,4 +135,4 @@ class Node extends React.Component{
     }
 }
 
-export default Node ;
+export default withRouter(Node) ;
