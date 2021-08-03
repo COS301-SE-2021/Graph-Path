48ac9174048885fdd4b54d07bbde08116fdc85dc
Kage-001 <73745316+Kage-001@users.noreply.github.com>
2021-08-03 15:17:19 +0200

    tests refactoring and skeleton update

---
+++ 1.Website/backend/src/test/Task.updateTask.test.js
@@ -0,0 +1,5 @@
+describe('/updateTaskDescription' , ()=>{})
+describe('/updateTaskStatus' , ()=>{})
+describe('/updateTaskDueDate' , ()=>{})
+describe('/updateTaskAssignee' , ()=>{})
+
--- 1.Website/backend/src/test/unit_testing/testingJest.test.js
+++
@@ -1,9 +0,0 @@
-describe('', function () {
-
-    it('should run', () => {
-
-        let x =5;
-        expect(x).toBe(5)
-    });
-
-});
