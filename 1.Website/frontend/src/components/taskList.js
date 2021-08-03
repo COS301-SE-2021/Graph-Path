dc8ad1e5e6803ebbd631a1b6d0acf3036f2f5ac8
Unabashed-Thabo <82472351+Unabashed-Thabo@users.noreply.github.com>
2021-08-03 11:24:35 +0200

    Update taskList.js
    api links

--- 1.Website/frontend/src/components/taskList.js
+++ 1.Website/frontend/src/components/taskList.js
@@ -2,8 +2,19 @@ import React from 'react' ;
 import { Link } from 'react-router-dom';
 
 class taskList extends React.Component{
+    constructor(props){
+        super(props);
+        this.state={
+            tasks: [],
+            isloading: false
+        },
+            api: "http://localhost:9001"
+    }
     render(){
-        
+
+        return (
+
+        )
     }
 }
 export default taskList ;
