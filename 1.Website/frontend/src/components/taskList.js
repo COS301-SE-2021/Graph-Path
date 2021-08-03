2df95259f66d615f3f8ddeeea9c1cc717c48e7e0
Unabashed-Thabo <82472351+Unabashed-Thabo@users.noreply.github.com>
2021-08-03 13:48:03 +0200

    Update taskList.js
    fetch tasks

--- 1.Website/frontend/src/components/taskList.js
+++ 1.Website/frontend/src/components/taskList.js
@@ -10,6 +10,37 @@ class taskList extends React.Component{
         },
             api: "http://localhost:9001"
     }
+
+    componentDidMount = ()=>{
+        this.viewTasksByProjects();
+    }
+
+    viewTasksByProjects = ()=>{
+
+        this.setState({
+           isloading:true
+        }) ;
+
+        fetch(`${this.state.api}/Task/getAllTasksByProjects/${}`)
+            .then(res=>res.json())
+            .then((result)=>{
+                const taskk = result ;
+                //No tasks to view
+                if (taskk.data === undefined ){
+                    this.setState({
+                        tasks:[],
+                        isloading:false
+                    }) ;
+                }
+                else{
+                    //some task found
+                    this.setState({
+                        tasks: taskk.data,
+                        isloading:false
+
+                    }) ;
+                })
+    }
     render(){
 
         return (
