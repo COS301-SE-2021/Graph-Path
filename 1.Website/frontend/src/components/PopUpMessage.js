53948d5df2490f04f1facf32980fa35373bf2d31
Livhuwani Mvhali <u17262578@tuks.co.za>
2021-08-08 18:37:28 +0200

    updated pop up file

--- 1.Website/frontend/src/components/PopUpMessage.js
+++ 1.Website/frontend/src/components/PopUpMessage.js
@@ -1,27 +1,14 @@
 import React from 'react';
 import '../css/PopUpMessage.css'
+import * as IoIcons from "react-icons/io5";
 
 
 class PopUpMessage extends React.Component{
-    constructor(props) {
-        super(props);
-        this.state ={
-            isHidden: false
-        }
-    }
-    //change popUp on parent
-    change =()=>{
-        this.setState({
-            isHidden: !this.state.isHidden
-        })
-    }
-
     render() {
-        const popUpText = this.props.text;
+        const popUpText = this.props.text
         return(
             <div  className="popUpContainer">
-                <p>{popUpText}</p>
-                <button onClick={this.change}>click</button>
+              <IoIcons.IoNotificationsCircleOutline className="notificationIcon" />  <p>{popUpText}</p>
             </div>
         )
     }
