f233b0b34bf6cfeca6d5ff307d67f5ac401107b4
Livhuwani Mvhali <u17262578@tuks.co.za>
2021-08-08 03:27:54 +0200

    updated pop up state

--- 1.Website/frontend/src/components/PopUpMessage.js
+++ 1.Website/frontend/src/components/PopUpMessage.js
@@ -1,11 +1,27 @@
 import React from 'react';
 import '../css/PopUpMessage.css'
 
+
 class PopUpMessage extends React.Component{
+    constructor(props) {
+        super(props);
+        this.state ={
+            isHidden: false
+        }
+    }
+    //change popUp on parent
+    change =()=>{
+        this.setState({
+            isHidden: !this.state.isHidden
+        })
+    }
+
     render() {
+        const popUpText = this.props.text;
         return(
-            <div className="popUpContainer">
-                <p>pop up under construction</p>
+            <div  className="popUpContainer">
+                <p>{popUpText}</p>
+                <button onClick={this.change}>click</button>
             </div>
         )
     }
