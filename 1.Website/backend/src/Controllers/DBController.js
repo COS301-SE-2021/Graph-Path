7e64240fd14659e69d18db25f8251338ac653b8a
Aliandro <u19016477@tuks.co.za>
2021-08-12 20:34:18 +0200

    added a comment

--- 1.Website/backend/src/Controllers/DBController.js
+++ 1.Website/backend/src/Controllers/DBController.js
@@ -3,7 +3,7 @@ const mongClient = require('mongodb').MongoClient ;
 require('dotenv').config() ;
 const DB_URI =process.env.TEST_DB_URI ; //|| 'mongodb://127.0.0.1:27017' ;
 
-console.log(DB_URI)
+//console.log(DB_URI)
 var db ;
 
 const dbController = {
