d0a3dbf703a1555875507f29e59fea546cd3664e
Kage-001 <73745316+Kage-001@users.noreply.github.com>
2021-08-14 01:54:39 +0200

    Code update : Finished prototype route to Convert project to kanban board

--- 1.Website/backend/src/Helpers/ScratchPad.js
+++ 1.Website/backend/src/Helpers/ScratchPad.js
@@ -1,16 +1,13 @@
 const {ObjectID: ObjectId} = require("mongodb");
 
-async function getProjectGraph(DBcontroller ,id)
-{
+async function getProjectGraph(DBcontroller ,id) {
 
-
-    let db =  DBcontroller.getConnectionInstance()
+    let db =  DBcontroller.getConnectionInstance();
     return await new Promise((resolve, reject)=>{
         db.collection('Projects').findOne({
             "_id": ObjectId(id)
         })
             .then((ans)=>{
-            console.log("Found");
             resolve(ans);
         })
             .catch(err=>{
@@ -22,24 +19,61 @@ async function getProjectGraph(DBcontroller ,id)
 
 }
 
-
-function updateNodesID(Project)
-{
-    let nodes = Project.graph.nodes;
-    for( var i = 0 ; i < nodes.length ; i ++)
+async function updateNodesID( DBcontroller,Project) {
+    if(Project !== {})
     {
-        let node = nodes[i];
-        node.uniqueID = Project['_id']+"_"+node.id;
-        Project.graph.nodes[i] = node;
+        let db =  DBcontroller.getConnectionInstance();
+
+        let nodes = Project.graph.nodes;
+        for( var i = 0 ; i < nodes.length ; i ++)
+        {
+            let node = nodes[i];
+            node.uniqueID = Project['_id']+"_"+node.id;
+            Project.graph.nodes[i] = node;
+
+        }
+       db.collection('Projects').updateOne({
+           "_id": ObjectId(Project['_id'])
+       },{
+           $set:{graph:Project.graph}
+       })
+           .then((ans)=>{
+               console.log("updated: ",Project['_id']);
+               return true;
+           })
+           .catch((err)=>{
+               console.log(err);
+               return false;
+           })
+
+
+
+
 
     }
+
     return Project;
 
 }
 
+function mapTaskToNode(DBcontroller,Task,Node) {
+    let db =  DBcontroller.getConnectionInstance();
+    db.collection('Tasks').updateOne({
+        "_id": ObjectId(Task['_id'])
+    },{
+            $set:{nodeID:Node.uniqueID}
+        })
+        .then((result)=>{
+            console.log("mapped Task to Node ")
+            return "mapped Task to Node";
+        })
+        .catch((err)=>{
+            console.log("failed to map task to Node");
+            return "failed to mapTask to Node";
+        })
+}
 
-function getNodes(Project)
-{
+function getNodes(Project) {
     let nodes = Project.graph.nodes;
     let returnedObj = [];
     if(nodes.length == 0)
@@ -63,16 +97,112 @@ function getNodes(Project)
 
 }
 
+async function getTasks(controller , Nodes) {
+    //------get All Nodes uniqueIDs--------------------
+    let db = controller.getConnectionInstance();
+    let uniqueIDs = [];
+    for (let i = 0; i < Nodes.length; i++) {
+        uniqueIDs.push(Nodes[i].uniqueID);
+    }
 
-function getTasks(Nodes)
-{
+    //------get All Tasks with uniqueIDs--------------------
+    return await new Promise((resolve, reject) => {
+        db.collection('Tasks').find({}).toArray()
+            .then((tasks) => {
+
+                if (tasks.length == 0) {
+                    resolve("Tasks collection empty");
+                } else {
+
+                    let AllTasks = [];
+                    for (var i = 0; i < tasks.length; i++) {
+
+                        //console.log("ln 109: ",tasks[i].nodeID);
+                        if (uniqueIDs.includes(tasks[i].nodeID)) {
+
+                            AllTasks.push(tasks[i]);
+                        }
+
+                    }
+
+                    resolve(AllTasks);
+
+                }
+
+            })
+            .catch((err) => {
+                console.log("error has occurs:", err)
+                reject("error getting tasks");
+            })
 
 
+        //return "get Task failed";
+
+    })
 }
 
+function splitTasksByStatus(Tasks)
+{
+    let InProgress = [];
+    let Complete = [];
+    let NotStarted =[];
+    for(let i = 0 ; i < Tasks.length ; i++)
+    {
+        if(Tasks[i].status ==="complete")
+        {
+            Complete.push(Tasks[i]);
+        }
+
+        else if(Tasks[i].status ==="in-progress")
+        {
+            InProgress.push(Tasks[i]);
+        }
+
+        else if(Tasks[i].status ==="notStarted")
+        {
+            NotStarted.push(Tasks[i]);
+        }
+
+    }
+    let total = Complete.length + InProgress.length + NotStarted.length;
+    let completePercentage,inProgressPercentage,notStartedPercentage;
+    if(total == 0)
+    {
+        completePercentage = 0.0;
+        inProgressPercentage = 0.0;
+        notStartedPercentage= 0.0;
+    }
+    else
+    {
+        completePercentage = (Complete.length/total);
+        inProgressPercentage = ( InProgress.length/total);
+        notStartedPercentage = (NotStarted.length/total);
+
+
+    }
+
+
+    return {
+        stats:{
+            completePercent : completePercentage,
+            inProgressPercent: inProgressPercentage,
+            notStartedPercent: notStartedPercentage
+
+        },
+        completeTasks : Complete,
+        inProgressTasks: InProgress,
+        notStartedTasks : NotStarted,
+    }
+
+}
 
 module.exports = {
     getProjectGraph,
     getNodes,
     updateNodesID,
+    getTasks,
+    mapTaskToNode,
+    splitTasksByStatus
 }
+
+/* notes: Now we need to include the uniqueNode ID for every task*/
