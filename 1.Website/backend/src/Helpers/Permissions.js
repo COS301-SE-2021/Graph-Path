13c1016274a4b5aacde6bcbc0c8d71a34e9738bb
Kage-001 <73745316+Kage-001@users.noreply.github.com>
2021-08-13 13:08:09 +0200

    Code update: added more roles and functions to map roles to permissions

--- 1.Website/backend/src/Helpers/Permissions.js
+++ 1.Website/backend/src/Helpers/Permissions.js
@@ -38,9 +38,22 @@ function getPermissions(role)
 
         case "owner":
             var Permissions = [
-                'edit',
-                'view',
-                'create-graph'
+                'Assign Manager',
+                'Change Manager',
+                'Assign user to task',
+                'Remove  user from task',
+                'Create Project',
+                'View project Graph',
+                'Attach due date tp task',
+                'Change due date of Task',
+                'Create task',
+                'Edit task',
+                'Remove task',
+                'Create graph node',
+                'Drag graph nodes',
+                'Save Graph changes',
+                'Change task status',
+                'Edit Member permissions',
             ];
             return Permissions
             break;
@@ -54,6 +67,39 @@ function getPermissions(role)
 }
 
 
+function getAllRoles()
+{
+    const Roles = [
+
+        'Project Manager',
+        'Client',
+        'Owner',
+        'scrum-master',
+        'developer',
+        'Tester',
+        'Business analyst',
+        'Tech Lead',
+        'QA engineer',
+    ]
+
+    return Roles;
+}
+
+function getAllRolesAndPermissions()
+{
+    let Roles = getAllRoles();
+    let RolesAndPerms = {};
+    for (let i = 0; i < Roles.length ; i++) {
+        RolesAndPerms.Roles[i] = getPermissions(Roles[i]);
+
+    }
+    return RolesAndPerms;
+}
+
+
+
 module.exports = {
     getPermissions,
+    getAllRoles,
+    getAllRolesAndPermissions
 }
