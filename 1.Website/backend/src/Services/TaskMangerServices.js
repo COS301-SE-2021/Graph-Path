e82b08b902e4a63e8b401a6d0b9bdf5161030f5a
Aliandro <u19016477@tuks.co.za>
2021-08-13 22:09:39 +0200

    added all of the functions

---
+++ 1.Website/backend/src/Services/TaskMangerServices.js
@@ -0,0 +1,207 @@
+const bcrypt = require("bcrypt");
+const ObjectId = require('mongodb').ObjectID;
+
+/////////////////////////////////////////////////////-Task-//////////////////////////////////////////////////////////////
+//***************************************************-get-**************************************************************
+
+async function getAllTasks(dbController){
+    const db = dbController.getConnectionInstance(dbController);
+    return await new Promise((resolve,reject)=>{
+        db.collection('Tasks').find({}).toArray()
+            .then(ans=>{
+                if(ans == null){
+                    resolve("No available tasks");
+                }else{
+                    resolve(ans);
+                }
+            })
+            .catch(err=>{
+                reject(err);
+            })
+    })
+}
+
+async function getTaskByID(dbController, id){
+    const db = dbController.getConnectionInstance();
+    return await new Promise((resolve, reject)=>{
+        db.collection('Tasks').findOne({
+            "_id": ObjectId(id)
+        })
+            .then(ans=>{
+                if(ans == null){
+                    resolve("No available task");
+                }else{
+                    resolve(ans);
+                }
+            })
+            .catch(err=>{
+                reject(err);
+            })
+    })
+}
+
+async function getAllTasksByProject(dbController, id){
+    const db = dbController.getConnectionInstance();
+    return await new Promise((resolve,reject)=>{
+        db.collection('Tasks').find({project:id}).toArray()
+            .then((ans)=>{
+                if(ans == null){
+                    resolve("No tasks found");
+                }else{
+                    resolve(ans);
+                }
+            })
+            .catch((err)=>{
+                reject(err);
+            });
+    })
+}
+
+//***************************************************-post-**************************************************************
+async function insertTask(dbController, taskObject){
+    const db = dbController.getConnectionInstance();
+    return await new Promise((resolve, reject)=>{
+        if(taskObject==null){
+            resolve("Task object empty");
+        }
+        db.collection('Tasks').insertOne(taskObject)
+            .then((ans)=>{
+                resolve(ans);
+            })
+            .catch(err=>{
+                reject(err);
+            })
+    })
+}
+//***************************************************-delete-**************************************************************
+async function deleteTaskByID(dbController, id){
+    const db = dbController.getConnectionInstance();
+    return await  new Promise((resolve,reject)=>{
+        db.collection('Tasks').deleteOne({
+            "_id":ObjectId(id)
+        })
+            .then(ans=>{
+                resolve(ans);
+            })
+            .catch(err=>{
+                reject(err);
+            })
+    })
+}
+//***************************************************-patch-**************************************************************
+async function updateTaskDescription(dbController, id, newDesc){
+    const db = dbController.getConnectionInstance();
+    return await new Promise((resolve, reject)=>{
+        db.collection('Tasks').updateOne({
+            "_id": ObjectId(id)
+        },{
+            $set:{description:newDesc}
+        })
+            .then(ans=>{
+                resolve(ans);
+            })
+            .catch(err=>{
+                reject(err);
+            })
+    })
+}
+
+async function updateTaskStatus(dbController, id, newStat){
+    const db = dbController.getConnectionInstance();
+    return await new Promise((resolve, reject)=>{
+        db.collection('Tasks').updateOne({
+            "_id": ObjectId(id)
+        },{
+            $set:{status:newStat}
+        })
+            .then(ans=>{
+                if(ans.modifiedCount > 0){
+                    resolve("Success");
+                }else{
+                    resolve("Could not update the task");
+                }
+            })
+            .catch(err=>{
+                reject(err);
+            })
+    })
+}
+/*
+async function updateTaskDueDate(id, ddate){
+    return await new Promise((resolve,reject)=>{
+        db.collection('Tasks').updateOne({
+            "_id": ObjectId(id)
+        },{
+            $set:{status:ddate}
+        })
+            .then(ans=>{
+                if(ans.modifiedCount > 0){
+                    resolve("Success");
+                }else{
+                    resolve("Could not update the task");
+                }
+            })
+            .catch(err=>{
+                reject(err);
+            })
+    })
+}
+
+ */
+
+async function updateTaskAssignee(dbController, id, assignee){
+    const db = dbController.getConnectionInstance();
+    return await new Promise((resolve,reject)=>{
+        db.collection('Tasks').updateOne({
+            "_id": ObjectId(id)
+        },{
+            $set:{assignee:assignee}
+        })
+            .then(ans=>{
+                if(ans.modifiedCount > 0){
+                    resolve("Success");
+                }else{
+                    resolve("Could not update the task");
+                }
+            })
+            .catch(err=>{
+                reject(err);
+            })
+    })
+}
+
+async function updateTaskAssigner(dbController, id, assigner){
+    const db = dbController.getConnectionInstance();
+    return await new Promise((resolve,reject)=>{
+        db.collection('Tasks').updateOne({
+            "_id": ObjectId(id)
+        },{
+            $set:{assigner:assigner}
+        })
+            .then(ans=>{
+                if(ans.modifiedCount > 0){
+                    resolve("Success");
+                }else{
+                    resolve("Could not update the task");
+                }
+            })
+            .catch(err=>{
+                reject(err);
+            })
+    })
+}
+
+
+module.exports={
+    //Task
+    getAllTasks,
+    getTaskByID,
+    getAllTasksByProject,
+    insertTask,
+    deleteTaskByID,
+    updateTaskDescription,
+    updateTaskStatus,
+    //updateTaskDueDate,
+    updateTaskAssigner,
+    updateTaskAssignee
+}
