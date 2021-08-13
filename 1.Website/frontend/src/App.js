c461e5fcecd285c8d8b2180860327bdc83099eb0
Unabashed-Thabo <82472351+Unabashed-Thabo@users.noreply.github.com>
2021-08-13 12:42:53 +0200

    Updated landing page

--- 1.Website/frontend/src/App.js
+++ 1.Website/frontend/src/App.js
@@ -1,7 +1,14 @@
 import React from 'react';
 import './css/App.css';
 import './css/Login.css'
-import Header from './components/Header' ; 
+import Header from './components/Header' ;
+import Register from './components/Register';
+import Login from './components/Login' ;
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.min.js';
+import 'jquery/dist/jquery.min.js';
+import $ from 'jquery';
+
 /*Changed this to class type*/
 class App extends React.Component {
 
@@ -9,9 +16,12 @@ class App extends React.Component {
     super(props) ; 
     this.state = {
       logged: false,
-      api:"http://localhost:9001"
+      api:"http://localhost:9001",
+      isLogged: true
     }
   }
+
+
 
   logInValid = () =>{
     // console.log('App must login') ;
@@ -33,27 +43,91 @@ class App extends React.Component {
   //   }) ; 
   // }
 
+componentDidMount()
+{
+  $(document).ready(function(){
+    $('.login-info-box').fadeOut();
+    $('.login-show').addClass('show-log-panel');
 
+
+    $('input[type="radio"]').on('change',function() {
+
+      if($('#log-reg-show').is(':checked')) {
+        $('.register-info-box').fadeIn();
+        $('.login-info-box').fadeOut();
+
+        $('.white-panel').removeClass('right-log');
+
+        $('.login-show').addClass('show-log-panel');
+        $('.register-show').removeClass('show-log-panel');
+      }
+      if($('#log-login-show').is(':checked')) {
+        $('.register-info-box').fadeOut();
+        $('.login-info-box').fadeIn();
+
+        $('.white-panel').addClass('right-log');
+        $('.register-show').addClass('show-log-panel');
+        $('.login-show').removeClass('show-log-panel');
+
+      }
+    });
+  });
+
+}
 
   render(){
-    const url = this.state.api ; 
-    if (this.state.logged){
-     
+    const url = this.state.api ;
+
+
+
+    return(
+        <div className="App">
+          <Header api={url}/>
+
+              <div className="login-reg-panel">
+                  <div className="login-info-box">
+                    <h2>Have an account</h2>
+                    <label id="label-register" htmlFor="log-reg-show">Sign In</label>
+                    <input type="radio" name="active-log-panel" id="log-reg-show" value="log-reg-show" />
+                  </div>
+
+                <div className="register-info-box">
+                  <h2>Don't have an account?</h2>
+                  <label id="label-login" htmlFor="log-login-show">Sign Up</label>
+                  <input type="radio" name="active-log-panel" value="log-login-show" id="log-login-show" />
+                  </div>
+                <div className="white-panel">
+                  <div className="login-show">
+                  <Login/>
+                  </div>
+                    <div className="register-show">
+                      <Register/>
+                    </div>
+                </div>
+
+
+
+              </div>
+
+        </div>
+    )
+    /*if (this.state.logged){
+
       return (
         <div className="App">
         <Header
         api={url}
         logOut={this.logOffValid} />
       </div>
-      ) ; 
+      ) ;
     }
     else{
 
       return (
           <div className="App">
-            <Header status="Please sign in, or sign up for an account" 
+            <Header status="Please sign in, or sign up for an account"
             logInValid={this.logInValid}
-            api={url} 
+            api={url}
             />
           <div>
             <img src={url} alt="img1" />
@@ -61,9 +135,11 @@ class App extends React.Component {
         </div>
       );
   }
+*/
 }
 
 }
+
 
 export default App;
 
