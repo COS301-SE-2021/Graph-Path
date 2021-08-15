ebd23186abe7fee59cc85aa574984270c1d9b8af
Livhuwani Mvhali <u17262578@tuks.co.za>
2021-08-16 01:01:48 +0200

    added functionality to updating project information

--- 1.Website/frontend/src/components/ProjectView.js
+++ 1.Website/frontend/src/components/ProjectView.js
@@ -2,7 +2,12 @@ import React from 'react' ;
 import { Link } from 'react-router-dom';
 import '../css/common.css' ;
 import '../css/ProjectView.css'
+import PopUpMessage from "./PopUpMessage";
 import {Button, Offcanvas} from "react-bootstrap";
+import * as HiIcons from "react-icons/hi";
+import axios from "axios";
+
+
 
 //Receives as a prop the project to display
 class ProjectView extends React.Component{
@@ -11,7 +16,15 @@ class ProjectView extends React.Component{
         this.state={
             edit:false,
             show: false,
-
+            projName:'',
+            projectOwner:'',
+            startD: '',
+            dueD:'',
+            empty : false,
+            api:'http://localhost:9001',
+            answer:'',
+            popUp: false,
+            popUpText: "",
         }
     }
 
@@ -27,36 +40,162 @@ class ProjectView extends React.Component{
         })
     }
 
+    /* disable and enable the pop up*/
+    showPopUP = () =>{
+        this.setState({
+            popUp: true
+        })
+        setTimeout(
+            () =>
+                this.setState({
+                    popUp: false
+                }),5000
+        );
+    }
+
+    change = (e)=>{
+        e.preventDefault();
+        const {name,value} = e.target;
+
+        switch(name){
+            case 'projName':
+                this.setState({
+                    empty: value.length === 0
+                })
+                break;
+
+            case 'startDate':
+                this.setState({
+
+                })
+        }
+        this.setState({ [name]: value })
+
+    }
+
+    onSubmit = (e)=>{
+        e.preventDefault();
+        console.log("submitted",this.state)
+
+        const data = {
+            projectName:'',
+            dueDate: '',
+            startDate: '',
+            owner: this.props.projectToDisplay.owner,
+            graph: this.props.projectToDisplay.graph,
+            groupMembers: this.props.projectToDisplay.groupMembers
+        }
+
+        console.log("props",this.props.projectToDisplay)
+        if(this.state.empty === true){
+
+        }else{
+            if(this.state.projName === ''){
+                data.projectName = this.props.projectToDisplay.projectName; //no change
+            }else{
+                data.projectName = this.state.projName; //change
+            }
+
+            if(this.state.startD === ''){
+                data.startDate = this.props.projectToDisplay.startDate;
+            }else{
+               data.startDate = this.state.startD;
+            }
+
+            if(this.state.dueD === ''){
+              data.dueDate = this.props.projectToDisplay.dueDate;
+            }else{
+               data.dueDate = this.state.dueD;
+            }
+            this.sendData(data);
+            console.log("data",data)
+            this.setState({
+                edit: false
+            })
+
+            this.setState({
+                popUpText: "User Information Updated"
+            });
+            this.showPopUP()
+        }
+
+    }
+
+
+    sendData = (data)=>{
+        try{
+            axios.put(`${this.state.api}/project/updateEverythingProject/${this.props.projectToDisplay._id}`,data)
+                .then((response)=>{
+                    if(response.status === 400){
+                        throw Error(response.statusText);
+                    }
+
+                    const res = response.data;
+
+                    this.setState({
+                        answer: res.message
+                    },()=>{
+                        if (this.state.answer !== undefined) {
+                            //alert(`Username or Password changed `)
+                            //this.props.updateUser(data)
+
+                        } else {
+                            alert(`Something went wrong please update again `)
+                        }
+                    })
+                },(response)=>{
+                    console.log('rejected', response);
+                    alert('Server Error, Please try again later');
+                })
+        }catch (error){
+            console.log(error)
+        }
+    }
+
 
     viewProject = (project,permissions)=>{
         console.log('project view',project,permissions)
         return(
             <div id="view-div">
                  <div id="project-form-div">
-                    <form id="project-form">
+                    <form id="project-form" onSubmit={this.onSubmit}>
                         <h3 style={{fontWeight:"bold"}}>Project Information</h3>
                         <label>Project Name</label>
                         <input
                             type='text'
-                            defaultValue={project.projectName} disabled />
+                            name = "projName"
+                            defaultValue={project.projectName}
+                            placeholder="Enter Project Name"
+                            onChange={this.change}
+                            required
+                             />
 
                         <label>Project Owner</label>
                         <input
                             type='text'
-                            value={project.owner} disabled/>
+                            value={project.owner}
+                            disabled/>
 
                         <label>Start Date</label>
                         <input
-                            type='text'
-                            defaultValue={project.startDate} disabled />
+                            type='date'
+                            name = "startD"
+                            onChange={this.change}
+                            defaultValue={project.startDate}
+                            required
+                        />
 
                         <label>Due Date</label>
                         <input
-                            type='text'
-                            defaultValue={project.dueDate} disabled />
+                            type='date'
+                            name = "dueD"
+                            onChange={this.change}
+                            defaultValue={project.dueDate} required />
 
-                        <input disabled={this.state.edit} type="button" id="editBtn" value="Edit" 
-                        onClick={this.toggleView}/>
+
+                        <input type="submit" id="editBtn" value="Submit"
+                                />
+
                     </form>
                      <br/>
 
@@ -76,13 +215,17 @@ class ProjectView extends React.Component{
                          <Offcanvas.Body>
                              {project.groupMembers.map((value,index)=>{
                                  console.log('from members',value)
-                                 return <div key={index}>{value.email} {value.label}</div>
+                                 return (
+                                     <div key={index}><p>Full names: {value.label}<br/>Email: {value.email}</p>
+                                         <HiIcons.HiUserRemove />
+                                         <hr/>
+
+                                     </div>
+                                 )
                              })}
+                             <HiIcons.HiUserAdd />
                          </Offcanvas.Body>
                      </Offcanvas>
-
-
-
                 </div>
 
             </div>
@@ -91,10 +234,10 @@ class ProjectView extends React.Component{
     }
 
 
-    toggleView = ()=>{
+    editView = ()=>{
         console.log('change view',this.state.edit)
         this.setState({
-            edit:!this.state.edit
+            edit: true
         })
     }
 
@@ -117,8 +260,9 @@ class ProjectView extends React.Component{
                 {
                  this.viewProject(project,EditPermissionRoles) 
                 }
-               
+                {this.state.popUp && <PopUpMessage text={this.state.popUpText} />}
             </div>
+
         )
         else{
 
