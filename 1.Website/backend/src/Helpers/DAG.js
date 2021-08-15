0b3dcb673d7f1bbaa390cd8b3c101695cc717715
Kage-001 <73745316+Kage-001@users.noreply.github.com>
2021-08-15 16:35:14 +0200

    Code update :added a setNodes function to take graph object from graph drawing library and reduce nodes to simpler objects

---
+++ 1.Website/backend/src/Helpers/DAG.js
@@ -0,0 +1,38 @@
+function SetNodes(graph)
+{
+    let Nodes = [];
+    let Edges = graph.edges;
+
+    // setting the Nodes data Structure
+    for( let i = 0 ; i < graph.nodes.length; i++ )
+    {
+        let NodeObj = {
+            id: graph.nodes[i].id,
+            adjacent : [],
+            weight: 0,
+            isVisit: false,
+        }
+        Nodes.push(NodeObj);
+
+    }
+
+    // setting Adjacent Nodes neighbours
+    for( let i =0 ; i < Edges.length ; i ++)
+    {
+
+        let sourceID = Edges[i].source;
+        let targetID = Edges[i].target;
+        let TargetNode = getNodeByID(targetID,Nodes);
+        for( let k = 0 ; k < Nodes.length ; k ++)
+        {
+            if( Nodes[k].id == sourceID)
+            {
+                Nodes[k].adjacent.push(TargetNode.id);
+            }
+        }
+
+    }
+
+    return Nodes;
+
+}
