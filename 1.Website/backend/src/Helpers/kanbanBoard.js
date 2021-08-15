4ee76ccb3c24fdd468501cc2f471f52dc852626c
Kage-001 <73745316+Kage-001@users.noreply.github.com>
2021-08-15 16:45:37 +0200

    Code update : added a function "isAcyclic" as a single function that returns true or false

--- 1.Website/backend/src/Helpers/kanbanBoard.js
+++ 1.Website/backend/src/Helpers/kanbanBoard.js
@@ -142,8 +142,7 @@ async function getTasks(controller , Nodes) {
     })
 }
 
-function splitTasksByStatus(Tasks)
-{
+function splitTasksByStatus(Tasks) {
     let InProgress = [];
     let Complete = [];
     let NotStarted =[];
@@ -197,13 +196,177 @@ function splitTasksByStatus(Tasks)
 
 }
 
+
+function isAcyclic(graph)
+{
+
+    if (graph.nodes.length == 0)
+        return false;
+
+    else
+    {
+
+        let stack = [];
+        let Nodes = SetNodes(graph);
+
+
+        for( let i = 0 ; i < Nodes.length ; i ++)
+        {
+            //console.log(Nodes);
+            if(isAcyclicRec(Nodes[i],stack,Nodes))
+            {
+                console.log("Graph is acyclic");
+                return true;
+            }
+        }
+        console.log("Graph is not acyclic");
+        return false;
+
+
+        /*console.log(Nodes);
+        VisitNode('n1',Nodes);
+        console.log(Nodes);
+
+
+        stack.push(Nodes[0]);
+        stack.push(Nodes[1]);
+        console.log(stack);
+        removeNodeFromStack(Nodes[0],stack);*/
+
+
+
+    }
+
+
+}
+
+function isAcyclicRec(node,stack,Nodes)
+{
+
+    if(isNodeInStack(node,stack))
+        return true;
+
+    if(node.isVisit === true)
+        return false;
+
+    VisitNode(node.id,Nodes);
+    stack.push(node);
+
+    let children = node.adjacent;
+    for( let i =0 ; i  < children.length ; i++)
+    {
+
+        if(isAcyclicRec(getNodeByID(children[i],Nodes),stack,Nodes))
+            return true;
+    }
+    removeNodeFromStack(node,stack);
+    return false;
+
+
+}
+
+
+function SetNodes(graph)
+{
+    let Nodes = [];
+    let Edges = graph.edges;
+
+    // setting the Nodes data Structure
+    for( let i = 0 ; i < graph.nodes.length; i++ )
+    {
+        let NodeObj = {
+            id: graph.nodes[i].id,
+            adjacent : [],
+            weight: 0,
+            isVisit: false,
+        }
+        Nodes.push(NodeObj);
+
+    }
+
+    // setting Adjacent Nodes neighbours
+    for( let i =0 ; i < Edges.length ; i ++)
+    {
+
+        let sourceID = Edges[i].source;
+        let targetID = Edges[i].target;
+        let TargetNode = getNodeByID(targetID,Nodes);
+        for( let k = 0 ; k < Nodes.length ; k ++)
+        {
+            if( Nodes[k].id == sourceID)
+            {
+                Nodes[k].adjacent.push(TargetNode.id);
+            }
+        }
+
+    }
+
+    return Nodes;
+
+}
+
+function getNodeByID(id,Nodes)
+{
+    for( let i =0 ; i < Nodes.length; i ++)
+    {
+
+        if(Nodes[i].id == id)
+        {
+
+            return Nodes[i];
+        }
+    }
+
+
+}
+
+function isNodeInStack(node, stack)
+{
+
+    return stack.includes(node);
+}
+
+
+function removeNodeFromStack(node,stack)
+{
+
+    for( let i =0 ; i < stack.length ; i ++)
+    {
+
+        if(stack[i] == node)
+        {
+            stack[i] =null;
+        }
+    }
+
+}
+
+
+function VisitNode(id, Nodes)
+{
+
+    for( let i =0 ; i < Nodes.length; i ++)
+    {
+
+        if(Nodes[i].id === id)
+        {
+
+           Nodes[i].isVisit = true;
+           return;
+        }
+    }
+
+}
+
+
 module.exports = {
     getProjectGraph,
     getNodes,
     updateNodesID,
     getTasks,
     mapTaskToNode,
-    splitTasksByStatus
+    splitTasksByStatus,
+    isAcyclic,
 }
 
 /* notes: Now we need to include the uniqueNode ID for every task*/
