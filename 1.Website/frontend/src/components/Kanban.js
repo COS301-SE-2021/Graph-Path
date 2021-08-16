5d14d2159af2b87cb50423f0a0783652c2b5265d
Unabashed-Thabo <82472351+Unabashed-Thabo@users.noreply.github.com>
2021-08-16 17:35:41 +0200

    Update Kanban.js
    Incomplete Kanban

--- 1.Website/frontend/src/components/Kanban.js
+++ 1.Website/frontend/src/components/Kanban.js
@@ -1,12 +1,20 @@
 import React from 'react'
 import '../css/Kanban.css'
-
+import {KanbanComponent, ColumnsDirective, ColumnDirective} from '@syncfusion/ej2-react-kanban';
 
 class Kanban extends React.Component{
+   render(){
+        return(
+            <KanbanComponent id="kanban" keyField="Status">
+                <ColumnsDirective>
+                    <ColumnDirective headerText="To Do" />
+                    <ColumnDirective headerText="In Progress" keyFiled="InProgressOpen"/>
+                    <ColumnDirective headerText="Review" keyFiled="/Review"/>
+                    <ColumnDirective headerText="Done" keyFiled="Close"/>
+                </ColumnsDirective>
 
-    render(){
-
-        return null;
+                </KanbanComponent>
+        )
 
     }
 }
