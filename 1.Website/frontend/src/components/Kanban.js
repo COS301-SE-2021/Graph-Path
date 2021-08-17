f9f07702be35d728ba7d2f69b33eca9d4e97ed1d
Unabashed-Thabo <82472351+Unabashed-Thabo@users.noreply.github.com>
2021-08-17 14:18:56 +0200

    Update Kanban.js
    update to kanban

--- 1.Website/frontend/src/components/Kanban.js
+++ 1.Website/frontend/src/components/Kanban.js
@@ -1,62 +1,48 @@
-// import * as React from 'react'
-// import '../css/App.css';
-// import '../css/Kanban.css';
-// import { KanbanComponent, ColumnsDirective, ColumnDirective } from "@syncfusion/ej2-react-kanban";
-// import {MockData} from './MockData';
-// import { extend } from '@syncfusion/ej2-base';
-// class Kanban extends React.Component {
-//     MockData: Object[] = [
-//         {
-//             Status: 'ToDo',
-//             Description: 'Fix Bugs',
-//             Assignee: 'Naane'
-//         },
-//         {
-//             Status: 'ToDo',
-//             Description: 'Work on Graph ',
-//             Assignee: 'Naane'
-//         },
-//         {
-//             Status: 'Done',
-//             Description: 'Something',
-//             Assignee: 'Baster'
-//         },
-//         {
-//             Status: 'InProgress',
-//             Description: 'Fix the UI issues',
-//             Assignee: 'Mvhali'
-//         },
-//         {
-//             Status: 'Review',
-//             Description: 'Fix the issues reported by the customer.',
-//             Assignee: 'Monareng'
-//         },
-//         {
-//             Status: 'Done',
-//             Description: 'Make the Kanban board and Link to database.',
-//             Assignee: 'Mndebele'
-//         },
-//         {
-//             Status: 'Validate',
-//             Description: 'Work on bugs',
-//             Assignee: 'Baster'
-//         }
-//     ];
-//     constructor() {
-//         super(...arguments);
-//         this.data = extend([], MockData, null, true);
-//     }
-//     render() {
-//         return (
-//             <KanbanComponent id="root" keyField="Status" dataSource={this.data} cardSettings={{ contentField: "Description", headerField: "Id" }} swimlaneSettings={{ keyField: "Assignee" }}>
-//                 <ColumnsDirective>
-//                     <ColumnDirective headerText="To Do" keyField="ToDo"/>
-//                     <ColumnDirective headerText="In Progress" keyField="InProgress"/>
-//                     <ColumnDirective headerText="Review" keyField="Review"/>
-//                     <ColumnDirective headerText="Done" keyField="Done"/>
-//                 </ColumnsDirective>
-//             </KanbanComponent>
-//         )
-//     }
-// }
-// export default Kanban;
+import * as React from 'react'
+import '../css/App.css';
+import '../css/Kanban.css';
+import { KanbanComponent, ColumnsDirective, ColumnDirective } from "@syncfusion/ej2-react-kanban";
+import {MockData} from './MockData';
+import { extend } from '@syncfusion/ej2-base';
+class Kanban extends React.Component {
+
+    constructor() {
+        super(...arguments);
+     //   this.myData =[];
+        this.data = extend([], MockData, null, true);
+
+    }
+  /*
+   componentDidMount=()=>{
+        this.theAPIData();
+}
+
+theAPIData =()=>{
+        this.setState({
+            loading:true
+        });
+        fetch('${this.state.api}/project/convertToKanbanBoard/:id/')
+            .then(res=>res.json())
+            .then((res)=>{
+                if(res.data!==undefined)
+                    this.myData=res.data;
+            })
+            .catch((err)=>{
+                console.log('An Error occurred',err)
+            })
+}
+
+   */
+    render() {
+        return (<KanbanComponent id="root" keyField="Status" dataSource={this.data} cardSettings={{contentField: "Description", headerField: "Id"}} swimlaneSettings={{ keyField: "Assignee"}}>
+                <ColumnsDirective>
+                    <ColumnDirective headerText="Not Started" keyField="notStartedTasks"/>
+                    <ColumnDirective headerText="In Progress" keyField="inProgressTasks"/>
+                    <ColumnDirective headerText="Complete" keyField="completeTasks"/>
+                </ColumnsDirective>
+            </KanbanComponent>
+        )
+    }
+}
+export default Kanban;
+
