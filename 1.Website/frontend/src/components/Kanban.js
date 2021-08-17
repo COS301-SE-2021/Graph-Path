17675d46e0e079aeba2961f3e33770d739abc501
Unabashed-Thabo <82472351+Unabashed-Thabo@users.noreply.github.com>
2021-08-17 14:53:02 +0200

    Update Kanban.js
    changed the Statuses

--- 1.Website/frontend/src/components/Kanban.js
+++ 1.Website/frontend/src/components/Kanban.js
@@ -1,38 +1,15 @@
 import * as React from 'react'
 import '../css/App.css';
 import '../css/Kanban.css';
-import { KanbanComponent, ColumnsDirective, ColumnDirective } from "@syncfusion/ej2-react-kanban";
+import { KanbanComponent, ColumnsDirective, ColumnDirective } from '@syncfusion/ej2-react-kanban';
 import {MockData} from './MockData';
-import { extend } from '@syncfusion/ej2-base';
+import {extend} from '@syncfusion/ej2-base';
 class Kanban extends React.Component {
 
     constructor() {
         super(...arguments);
-     //   this.myData =[];
         this.data = extend([], MockData, null, true);
-
     }
-  /*
-   componentDidMount=()=>{
-        this.theAPIData();
-}
-
-theAPIData =()=>{
-        this.setState({
-            loading:true
-        });
-        fetch('${this.state.api}/project/convertToKanbanBoard/:id/')
-            .then(res=>res.json())
-            .then((res)=>{
-                if(res.data!==undefined)
-                    this.myData=res.data;
-            })
-            .catch((err)=>{
-                console.log('An Error occurred',err)
-            })
-}
-
-   */
     render() {
         return (<KanbanComponent id="root" keyField="Status" dataSource={this.data} cardSettings={{contentField: "Description", headerField: "Id"}} swimlaneSettings={{ keyField: "Assignee"}}>
                 <ColumnsDirective>
