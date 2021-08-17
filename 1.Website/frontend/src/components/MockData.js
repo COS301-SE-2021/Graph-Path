00f06e7175631a162b2da8f1058d0adc8f7bcd01
Unabashed-Thabo <82472351+Unabashed-Thabo@users.noreply.github.com>
2021-08-17 15:18:09 +0200

    Update MockData.js
    updated IDs

--- 1.Website/frontend/src/components/MockData.js
+++ 1.Website/frontend/src/components/MockData.js
@@ -1,36 +1,43 @@
 export let MockData: Object[] = [
     {
+        Id:1,
         Status: 'inProgress',
         Description: 'Fix Bugs',
         Assignee: 'Naane'
     },
     {
+        Id: 2,
         Status: 'done',
         Description: 'Work on Graph ',
         Assignee: 'Naane'
     },
     {
+        Id: 3,
         Status: 'open',
         Description: 'Something',
         Assignee: 'Baster'
     },
     {
+        Id:4,
         Status: 'inProgress',
         Description: 'Fix the UI issues',
         Assignee: 'Mvhali'
     },
 
     {
+        Id:5,
         Status: 'done',
         Description: 'Fix the issues reported by the customer.',
         Assignee: 'Monareng'
     },
     {
+        Id:6,
         Status: 'open',
         Description: 'Make the Kanban board and Link to database.',
         Assignee: 'Mndebele'
     },
     {
+        Id:7,
         Status: 'done',
         Description: 'Work on bugs',
         Assignee: 'Baster'
