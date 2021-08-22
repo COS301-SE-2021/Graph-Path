45653eb2df626d85b4b1bff550ce547170088b84
Unabashed-Thabo <u16103948@tuks.co.za>
2021-08-22 13:10:31 +0200

    Update Register.js
    Form Inputs

--- 1.Website/frontend-v2/graph-path/src/components/Register.js
+++ 1.Website/frontend-v2/graph-path/src/components/Register.js
@@ -1,8 +1,37 @@
 import React from 'react'
+import { Form, FormGroup, FormControl, ControlLabel} from 'rsuite';
 
 class Register extends React.Component{
+
     render(){
-        return <h1> Do you print</h1>
+        return (
+            <Form >
+                <FormGroup>
+                    <ControlLabel> FirstName</ControlLabel>
+                    <FormControl name="firstName" type="name"/>
+                </FormGroup>
+
+                <FormGroup>
+                    <ControlLabel> LastName</ControlLabel>
+                    <FormControl name="lastName" type="name"/>
+                </FormGroup>
+
+                <FormGroup>
+                    <ControlLabel> Username</ControlLabel>
+                    <FormControl name="userName"/>
+                </FormGroup>
+
+                <FormGroup>
+                    <ControlLabel> Email</ControlLabel>
+                    <FormControl name="email" type="email"/>
+                </FormGroup>
+
+                <FormGroup>
+                    <ControlLabel> Password</ControlLabel>
+                    <FormControl name="password" type="password"/>
+                </FormGroup>
+            </Form>
+        )
     }
 }
 
