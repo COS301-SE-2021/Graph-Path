813001a485ffe74c450d787b35ed9803b2bbd623
Unabashed-Thabo <u16103948@tuks.co.za>
2021-08-22 14:39:13 +0200

    Update Header.js
    Added SignUp from header

--- 1.Website/frontend-v2/graph-path/src/components/Header.js
+++ 1.Website/frontend-v2/graph-path/src/components/Header.js
@@ -1,21 +1,37 @@
 import {React,Component} from "react";
-import { Container, Header, Content, Footer, Sidebar } from 'rsuite';
+import {  Modal, Button } from 'rsuite';
 import "rsuite/dist/styles/rsuite-default.css" ;
+import Register from './Register'
 
 class CustomHeader extends Component{
+
+    constructor(props){
+        super(props);
+        this.state={
+            show:false
+        }
+        this.close = this.close.bind(this);
+        this.open = this.open.bind(this);
+    }
+    open(){
+        this.setState({ show: true})
+    }
+    close(){
+        this.setState({show: false})
+    }
     render(){
         return (
-        <div>   
-             <Container>
-             Landing Page 
-             <br />
-                Log In | Register 
-
-                <Header>Header</Header>
-                <Content>Content</Content>
-                <Footer>Footer</Footer>
-             </Container>            
-        </div>      
+            <div>
+                <Modal   show={this.state.show} onHide={this.close} size="xs">
+                    <Modal.Header>
+                        <Modal.Title>Sign Up</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>
+                        <Register />
+                    </Modal.Body>
+                </Modal>
+                <Button onClick={this.open}>Sign Up</Button>
+            </div>
 
         )
     }
