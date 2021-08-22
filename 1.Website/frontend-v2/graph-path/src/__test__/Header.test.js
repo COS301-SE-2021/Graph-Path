ec3dae9c04463ca627c4da1ac3104f1f2354b784
nanitight <ntpnaane@gmail.com>
2021-08-22 17:40:01 +0200

    Test file for Header, App and Project Manager

--- 1.Website/frontend-v2/graph-path/src/__test__/Header.test.js
+++ 1.Website/frontend-v2/graph-path/src/__test__/Header.test.js
@@ -1,8 +1,49 @@
 import { render, screen } from '@testing-library/react';
 import Header from '../components/Header';
-
-test('renders learn react link', () => {
-  render(<Header />);
-  const linkElement = screen.getByTestId("tidHeader");
-  expect(linkElement).toNotBeEmpty();
+import ReactDOM from 'react-dom' ;
+import React from 'react' ;
+it('renders without crashing', () => {
+    render(<Header />);
+    const HeaderParent = document.createElement('div');   ; 
+    // React-Dom Render test
+    ReactDOM.render(<Header/>,HeaderParent) ;
+    
+    //testing library test
+    const {getByTestId} = render(<Header/>);
+    expect(getByTestId("tidHeader")).toNotBeEmpty();
 });
+
+it('should render a sign up link',()=>{
+    const {getByTestId} = render(<Header/>);
+    expect(getByTestId('tidSignUpLink')).toHaveTextContent('Sign Up')
+    
+
+}) ;
+
+it('should render a log in link',()=>{
+
+}) ;
+
+
+it('should render the dashboard when the user is logged in',()=>{
+
+}) ;
+
+// test for the opposite condition  
+
+it('should render nav bar for navigating when user is logged in',()=>{
+
+}) ;
+
+// test for the opposite condition  
+
+
+it('should render The project manager',()=>{
+
+}) ;
+
+
+
+it('',()=>{
+
+}) ;
---
+++ 1.Website/frontend-v2/graph-path/src/__test__/ProjectManager.test.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { ReactDOM } from 'react-dom';
+import {screen,cleanup} from '@testing-library/react' ;
+import ProjectManager from '../components/ProjectManager';
+
+
+afterEach(cleanup);
+
+const testDiv = document.createElement("div") ;
+it('Should render Project Manager without props',()=>{
+    ReactDOM.render(<ProjectManager/>,testDiv) ;
+}) ; 
+
+it('should render with all required props',()=>{
+    
+}) ; 
+
+const projects = [{
+    projectName:"Test 1",
+    lastDateAccessed: Date().today() ,
+    members:[]
+}]
+
+
+it('When given a set of projects, render at least one of the projects',()=>{
+    
+}) ; 
+
+it('When given a set of projects, it should render with most accessed project first',()=>{
+    
+}) ; 
+
+
+it('It should be able to display the projects with another sorting system available',()=>{
+    
+}) ; 
+
