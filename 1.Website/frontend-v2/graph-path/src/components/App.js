259f8e00c4503d1c634b3b28033982af02de5040
Unabashed-Thabo <u16103948@tuks.co.za>
2021-08-23 11:39:30 +0200

    Update App.js
    Changed Landing page

--- 1.Website/frontend-v2/graph-path/src/components/App.js
+++ 1.Website/frontend-v2/graph-path/src/components/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import '../css/App.css';
-
+import Landing from './Landing'
 
 //import Register from './Register'
 
@@ -14,7 +14,7 @@ class App extends React.Component {
     return (
       // <Router>
         <div className="App" data-testid="tidApp">    
-            <Header />
+            <Landing />
         </div>
       /* </Router> */
       
