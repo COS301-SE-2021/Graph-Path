a9c721de6dc88f83be3c6a6b3dd3fed024d1d9ae
Livhuwani Mvhali <u17262578@tuks.co.za>
2021-08-24 10:04:59 +0200

    added modal

---
+++ 1.Website/frontend-v2/graph-path/src/components/Modal.js
@@ -0,0 +1,16 @@
+import  React from 'react' ;
+
+
+class Modal extends React.Component{
+    render() {
+        return(
+            <>
+            <div>
+                <h1>Hello</h1>
+            </div>
+            </>
+        )
+    }
+}
+
+export default Modal;
