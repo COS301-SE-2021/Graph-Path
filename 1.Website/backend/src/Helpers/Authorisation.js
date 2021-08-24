0c934f864d9a952986a9e28adca223040d9ba032
Kage-001 <73745316+Kage-001@users.noreply.github.com>
2021-08-24 18:19:13 +0200

    code update : added Authorisation.js to authorise routes

---
+++ 1.Website/backend/src/Helpers/Authorisation.js
@@ -0,0 +1,32 @@
+function AuthoriseDeleteTask(req,res,next)
+{
+    console.log("checking permission to delete tasks")
+    const userProjects = req.user.projects;
+    const userProject = userProjects.filter(project => project.projectID === req.body.projectID);
+    if( userProject){
+        console.log(userProject[0])
+        const permissions = userProject[0].permissions;
+        if(permissions.includes("delete task")){
+            console.log("permitted to delete tasks")
+            next()
+
+        } else{
+            console.log("delete task authorization failed")
+            res.status(403).send({
+                message: "Unauthorised to delete tasks",
+            })
+        }
+    } else {
+        res.status(403).send({
+            message: "Not authorised to access current project"
+        })
+    }
+}
+
+
+function AuthoriseKanbanBoard(req,res,next){
+
+}
+module.exports = {
+    AuthoriseDeleteTask
+}
