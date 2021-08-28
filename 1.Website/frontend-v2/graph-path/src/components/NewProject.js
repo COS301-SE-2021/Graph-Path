ca151cb714338d5e6441b23c88617350bb84bfcb
Livhuwani Mvhali <u17262578@tuks.co.za>
2021-08-29 01:21:19 +0200

    code cleanup

--- 1.Website/frontend-v2/graph-path/src/components/NewProject.js
+++ 1.Website/frontend-v2/graph-path/src/components/NewProject.js
@@ -78,59 +78,8 @@ class NewProject extends React.Component{
                                     <HelpBlock tooltip>Required</HelpBlock>
                                 </FormGroup>
                             </Form>
-                            {/*:*/}
-                            {/*// <div id="permission-div">*/}
-                            {/*//*/}
-                            {/*//     <div id="role-div" >*/}
-                            {/*//*/}
-                            {/*//     </div>*/}
-                            {/*//     <div id="can-do-div">*/}
-                            {/*//*/}
-                            {/*//     </div>*/}
-                            {/*//*/}
-                            {/*// </div>*/}
-                            {/*<>*/}
-                            {/*    <FlexboxGrid id="permission-div" justify="space-around">*/}
-                            {/*        <FlexboxGrid.Item id="role-div" colspan={24} md={6} >*/}
-                            {/*            Roles*/}
-                            {/*            <Divider/>*/}
-
-                            {/*        </FlexboxGrid.Item>*/}
-                            {/*        <FlexboxGrid.Item id="can-do-div" colspan={24} md={6} >Permissions*/}
-                            {/*            <Divider/>*/}
-                            {/*            <div id="check-list">*/}
-                            {/*                <Checkbox>View Graph</Checkbox>*/}
-                            {/*                <Checkbox>Add Node</Checkbox>*/}
-                            {/*                <Checkbox>Add Task</Checkbox>*/}
-                            {/*                <Checkbox>Update Project Information</Checkbox>*/}
-                            {/*                <Checkbox>Delete Project</Checkbox>*/}
-                            {/*                <Checkbox>Delete Node</Checkbox>*/}
-                            {/*                <Checkbox>Remove Members</Checkbox>*/}
-                            {/*                <Checkbox>Add Members</Checkbox>*/}
-                            {/*                <Checkbox>Change Task Status</Checkbox>*/}
-                            {/*            </div>*/}
-                            {/*        </FlexboxGrid.Item>*/}
-                            {/*    </FlexboxGrid>*/}
-                            {/*</>*/}
-                    {/*}*/}
                 </Modal.Body>
                 <Modal.Footer>
-                 {/*{*/}
-                 {/*       this.state.next === false ?*/}
-
-                 {/*           <Button variant="primary" onClick={this.handleNext}>*/}
-                 {/*               Next*/}
-                 {/*           </Button>*/}
-                 {/*           :<>*/}
-                 {/*               <Button variant="primary" onClick={this.handleNext}>*/}
-                 {/*                   Back*/}
-                 {/*               </Button>*/}
-                 {/*               <Button variant="secondary" onClick={this.handleClose}>*/}
-                 {/*                   Create*/}
-                 {/*               </Button>*/}
-                 {/*           </>*/}
-                 {/*   }*/}
-
                     <Button variant="secondary" onClick={this.handleClose}>
                         Create
                     </Button>
