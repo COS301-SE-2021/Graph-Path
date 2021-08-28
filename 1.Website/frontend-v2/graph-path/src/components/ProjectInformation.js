72ff2b043f01ef2712ce19438fd08430b7055430
Livhuwani Mvhali <u17262578@tuks.co.za>
2021-08-29 00:48:51 +0200

    added project information

---
+++ 1.Website/frontend-v2/graph-path/src/components/ProjectInformation.js
@@ -0,0 +1,29 @@
+import React from 'react' ;
+import '../css/ProjectInformation.css'
+
+class ProjectInformation extends React.Component{
+    /**
+     * Update project information (name, due, start)
+     * Show(Members, )
+     * Add or Invite Members
+     * Remove Members
+     * Change roles
+     * @returns {JSX.Element}
+     */
+
+    render() {
+        const project = this.props.project;
+        return(
+            <div id="main-div">
+                <div>
+
+                </div>
+                <div>
+
+                </div>
+
+            </div>
+        )
+    }
+}
+export default ProjectInformation;
