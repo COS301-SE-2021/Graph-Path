2a41bbe24aed0395f7a9438728962e51340fa41b
nanitight <ntpnaane@gmail.com>
2021-08-28 13:22:38 +0200

    Structure for building

--- 1.Website/frontend-v2/graph-path/src/App.js
+++ 1.Website/frontend-v2/graph-path/src/App.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import '../css/App.css';
+import './css/App.css';
 
 //import Register from './Register'
 
-import Header from './Header' ;
+import Header from './components/Header' ;
 import {HashRouter as Router} from 'react-router-dom' ;
 
 
@@ -11,11 +11,11 @@ import {HashRouter as Router} from 'react-router-dom' ;
 class App extends React.Component {
     render(){
     return (
-      // <Router>
+      <Router>
         <div className="App" data-testid="tidApp">    
             <Header />
         </div>
-      //  </Router> 
+       </Router> 
       
     );
   }
--- 1.Website/frontend-v2/graph-path/src/index.js
+++ 1.Website/frontend-v2/graph-path/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './components/App';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
 import {Auth0Provider} from "@auth0/auth0-react";
 
