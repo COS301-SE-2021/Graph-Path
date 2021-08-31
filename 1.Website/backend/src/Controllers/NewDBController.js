ca73e59fc9dc41530662cc3a6d69854bfc06a77e
Aliandro <u19016477@tuks.co.za>
2021-08-31 16:34:33 +0200

    Cleaned up unnecessary and commented out code

--- 1.Website/backend/src/Controllers/NewDBController.js
+++ 1.Website/backend/src/Controllers/NewDBController.js
@@ -1,7 +1,7 @@
 const mongClient = require('mongodb').MongoClient ;
 require('dotenv').config() ;
-const DB_URI =process.env.TEST_DB_URI ;
-//const DB_URI = "mongodb+srv://NoCap2021:NoCap2021@cluster0.n67tx.mongodb.net/test?retryWrites=true&w=majority";
+//const DB_URI =process.env.TEST_DB_URI ;
+const DB_URI = "mongodb+srv://NoCap2021:NoCap2021@cluster0.n67tx.mongodb.net/test?retryWrites=true&w=majority";
 var ObjectId = require('mongodb').ObjectID;
 const mongoose = require('mongoose') ;
 
@@ -16,41 +16,12 @@ mongClient.connect(DB_URI, {useNewUrlParser:true,useUnifiedTopology:true},(err,c
     console.log('+++++++++++++++++++++++++++connect to db, to serve request++++++++++') ;
 })
 
-
-//connect to db
-// const dbController = {
-//     connect: (callback)=>{
-//         mongClient.connect(DB_URI,
-//             {useNewUrlParser:true,useUnifiedTopology:true},
-//             (err,clientDB)=>{
-//                 db = clientDB.db('test') ;
-//                 console.log('+++++++++++++++++++++++++++connect to db, to serve request++++++++++') ;
-//                 //return callback(err) ;
-//             }) ;
-//     },
-//     getDB : ()=>{
-//         console.log('returned mongoDB connection instance')
-//         return db ;
-//     }
-// }
-// dbController.connect();
-// dbController.getDB();
-
  function getConnectionInstance()
 {
     console.log("returned mongoDB instance");
     return db;
 }
 
-
-/////////////////////////////////////////////////////-Node-//////////////////////////////////////////////////////////////
-
-/////////////////////////////////////////////////////-Graph-//////////////////////////////////////////////////////////////
-
-
-
-/////////////////////////////////////////////////////-exports-//////////////////////////////////////////////////////////////
-// console.log(getUserByID);
 module.exports={
 
     getConnectionInstance
