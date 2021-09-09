de7b33cb8af66a9ef4b89d14628caa90fd3d3f16
Unabashed-Thabo <u16103948@tuks.co.za>
2021-09-09 05:37:03 +0200

    Profile test
    Profile test file added

--- 1.Website/frontend-v2/graph-path/src/components/Profile.js
+++ 1.Website/frontend-v2/graph-path/src/components/Profile.js
@@ -41,7 +41,7 @@ class Profile extends React.Component{
     }
 
     render() {
-        const picture = this.props.user.picture;
+        // const picture = this.props.user.picture;
         const user = this.props.user;
         return(
             <>
@@ -52,7 +52,8 @@ class Profile extends React.Component{
                     </Drawer.Header>
                     <Drawer.Body id="body-div">
                         <div id="picture-div">
-                            <img src={picture} />
+
+                            {/*<img src={picture} />*/}
                             <Divider/>
                             <h6>{this.props.user.name}</h6>
 
