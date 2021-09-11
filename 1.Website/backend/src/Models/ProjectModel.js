2440e141a5de4e4845656d3d100b010d5483acc9
Aliandro <u19016477@tuks.co.za>
2021-09-11 11:03:23 +0200

    Added a model for the project to follow

--- 1.Website/backend/src/Models/ProjectModel.js
+++ 1.Website/backend/src/Models/ProjectModel.js
@@ -1,18 +1,35 @@
 const mongoose = require('mongoose') ;
-// const db = require('../Controllers/DBController') ;
 
-const ProjectSchema = mongoose.Schema({
 
-    _id: mongoose.Schema.Types.ObjectId,
+const ProjectObjectScheme = {
+
+    //_id: mongoose.Schema.Types.ObjectId(),
     projectName: String,
     startDate: Date,
     dueDate: Date,
     groupMembers:Array,//list of members
-    owner: String,
-    userId : mongoose.Schema.Types.ObjectId
-});
+    projectOwner: String,
+    graph: Object,
+    lastAccessed: Date,
+    projectDescription: String,
+    status: String
 
-// module.exports  = mongoose.model("ProjectModel",ProjectSchema);
-// const ProjectModel = db.model('ProjectModel,', ProjectSchema,'Projects') ;  
-// module.exports = ProjectModel ;
-module.exports = ProjectSchema ;
+
+}
+
+const ProjectObjectBlank = {
+    //_id: mongoose.Schema.Types.ObjectId(),
+    projectName: "",
+    startDate: "",
+    dueDate: "",
+    groupMembers:[],//list of members
+    projectOwner: "",
+    graph: {},
+    lastAccessed: "",
+    projectDescription: "",
+    status: ""
+}
+module.exports ={
+    ProjectObjectScheme,
+    ProjectObjectBlank
+}
