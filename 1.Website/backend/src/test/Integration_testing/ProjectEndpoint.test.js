27422e1eb4b4c65b541afae5fab2fab921983603
Kage-001 <73745316+Kage-001@users.noreply.github.com>
2021-09-18 23:36:42 +0200

    integration testing  : added test case on ProjectEndpoint.test.js

--- 1.Website/backend/src/test/Integration_testing/ProjectEndpoint.test.js
+++ 1.Website/backend/src/test/Integration_testing/ProjectEndpoint.test.js
@@ -2,41 +2,99 @@ const makeApp = require('../../app');
 const supertest = require('supertest');
 const {MongoClient} = require('mongodb')
 const MockDBController = require('../../Controllers/MockDBController');
+
 const mongoose = require("mongoose");
 const ProjectmanagerService = require("../../Services/ProjectManagerService");
+//const mockProjects = require("../../src/test/MockCollections/mockProjects.json");
 
 describe('/statistics/RadarGraph/:projectID',()=> {
 
     describe("when requested", ()=>{
+
         let connection;
         let MockDB;
+        let mockMemberObject = {
+            email:"test@gmail.com",
+            permissions: ['owner']
+        }
+        let mockProject ={
+            _id:  new mongoose.mongo.ObjectID(),
+            projectOwner: "testOwner",
+            projectName:"testProject",
+            projectDescription: "testDescription",
+            startDate :"2021/01/01",
+            dueDate : "2021/01/01",
+            status: "not started",
+            groupMembers :[mockMemberObject],
+            graph: {},
+            lastAccessed: new Date(),
+
+        };
+        let mockUser= {
+
+        }
         beforeAll(async () => {
 
             connection = await MongoClient.connect(global.__MONGO_URI__, {
                 useNewUrlParser: true,
                 useUnifiedTopology: true
             });
-            MockDB = await MockDBController;
+            MockDB = await MockDBController.getConnectionInstance();
+            const Projects =  MockDB.collection('Projects');
+            const Tasks    =  MockDB.collection('Tasks');
+            const Users    =  MockDB.collection('Users');
 
-            const Projects = MockDB.getConnectionInstance().collection('Projects');
-            await Projects.insertOne(mockProject);
+            const MockProjectCollection = await require('../MockCollections/mockProjects.json');
+            const MockTaskCollection = await require('../MockCollections/mockTasks.json');
+            const MockUserCollection = await require('../MockCollections/mockUser.json');
+
+
+            let test_n_projects =[];
+            let test_n_users    =[];
+            let test_n_tasks    =[];
+
+            for (let i = 0; i <MockProjectCollection.length; i++) {
+                test_n_projects.push(MockProjectCollection[i]);
+                test_n_projects[i]._id = MockProjectCollection[i]._id.$oid.toString();
+
+            }
+
+            for (let i = 0; i <MockTaskCollection.length; i++) {
+                test_n_users.push(MockTaskCollection[i]);
+                test_n_users[i]._id = MockTaskCollection[i]._id.$oid.toString();
+
+            }
+            for (let i = 0; i <MockUserCollection.length; i++) {
+                test_n_tasks.push(MockUserCollection[i]);
+                test_n_tasks[i]._id = MockUserCollection[i]._id.$oid.toString();
+            }
+
+            await Projects.insertMany(test_n_projects);
+            await Projects.insertMany(test_n_users);
+            await Projects.insertMany(test_n_tasks);
 
 
         });
         afterAll(async () => {
-            await  ProjectmanagerService.removeProjectByID(MockDB, mockProject._id)
+            MockDB.collection('Projects').drop();
+            MockDB.collection('Tasks').drop();
+            MockDB.collection('Users').drop();
             await connection.close();
             await MockDB.close();
         });
 
+        describe('/projects/insert')
         it('it should return status code 200', async ()=> {
 
 
             let app = makeApp(MockDBController);
+            const projectID = mockProject._id;
             let response  = await supertest(app)
-                .get('/task/getAllTasks')
+                .get('/project/statistics/donutChart/'+projectID)
                 .expect(200)
-                .then((res)=>{  })
+                .then((res)=>{
+                    expect(res).toBe("null")
+                })
 
         });
         it('it should return a JSON object', async ()=> {
