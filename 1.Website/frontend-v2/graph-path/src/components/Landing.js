2c22b9e8f82c5e18b4d2595753e12c3b05d3d36a
Unabashed-Thabo <u16103948@tuks.co.za>
2021-10-13 09:16:53 +0200

    Landing page
    Small changes to landing

--- 1.Website/frontend-v2/graph-path/src/components/Landing.js
+++ 1.Website/frontend-v2/graph-path/src/components/Landing.js
@@ -1,19 +1,23 @@
 import React from 'react'
 //import landingSnap from "../img/landing.png";
-import {Button, Carousel, Loader} from "rsuite";
+import {Button, Loader} from "rsuite";
 import "rsuite/dist/styles/rsuite-dark.min.css"
 import '../css/Landing.css';
 import {useAuth0} from '@auth0/auth0-react';
 //import JSONPretty from 'react-json-pretty';
 import LoginBtn from "./LoginBtn";
 import Graph_Picture from "../img/graph_2.svg";
-import dashboard from "../img/dashboard.jpg";
-import createP from "../img/createp.jpg";
-import graph from "../img/graph.jpg";
-import stats1 from "../img/stats1.jpg";
-import stats2 from "../img/stats2.jpg";
-import stats3 from "../img/stats3.jpg";
-import kanban from "../img/kanban.jpg";
+//import dashboard from "../img/dashboard.jpg";
+//import createP from "../img/createp.jpg";
+//import graph from "../img/graph.jpg";
+//import stats1 from "../img/stats1.jpg";
+//import stats2 from "../img/stats2.jpg";
+//import stats3 from "../img/stats3.jpg";
+//import kanban from "../img/kanban.jpg";
+import sendMessage from "../img/landing/SendMessage.gif"
+import kanbanBoard from "../img/landing/SearchKanban.gif"
+import graphDrag from "../img/landing/GraphDrag.gif"
+//import CreateTask from "../img/gif/CreateTask.gif";
 
 function Landing({logInvalid}) {
 
@@ -24,23 +28,44 @@ function Landing({logInvalid}) {
         if(isLoading) return <Loader speed="fast" content="Loading" />
 
 
+
         return(
 
             <div data-testid="main-landing-id" id="main-div-landing">
                 <div id="left-div">
+
                         <img id="graph-picture" src={Graph_Picture} alt="graph-pic"/>
+                        <br/> <br/> <br/> <br/>
+                       <div >
+                               <h1>Features</h1>
+                               <br/> <br/>
+                               <h3>Collaborative Chat</h3>
+                               <p className="landingText">The Graph Path has a Collaborative Chat intended for project users to interact with one another</p> <br/> <br/>
+                               <img src={sendMessage} alt-='ChatGIF' />
 
-                        <Carousel id="carousel-id" autoplay shape='bar' className="custom-slider">
-                                <img src={dashboard} alt='' />
-                                <img src={createP} alt=''/>
-                                <img src={graph} alt=''/>
-                                <img src={kanban} alt=''/>
-                                <img src={stats1} alt=''/>
-                                <img src={stats2} alt=''/>
-                                <img src={stats3} alt=''/>
+                               <h3>Kanban Board</h3>
+                               <p className="landingText">Traditionally, the Kanban Board is used to display a single project. The Graph Path Kanban has an overview of all projects.</p> <br/> <br/>
+                               <img src={kanbanBoard} alt='KanbanGIF' />
+
+                               <h3>Graph View</h3>
+                               <p className="landingText">Tasks are organized as an interactive acyclic graph. The graph is user-friendly allowing for node linkage and dragging as the user wishes.</p>
+                               <img src={graphDrag} alt='GraphGIF'/>
+                       </div>
 
 
-                        </Carousel>
+
+
+                        {/*<Carousel id="carousel-id" autoplay shape='bar' className="custom-slider">*/}
+                        {/*        <img src={dashboard} alt='' />*/}
+                        {/*        <img src={createP} alt=''/>*/}
+                        {/*        <img src={graph} alt=''/>*/}
+                        {/*        <img src={kanban} alt=''/>*/}
+                        {/*        <img src={stats1} alt=''/>*/}
+                        {/*        <img src={stats2} alt=''/>*/}
+                        {/*        <img src={stats3} alt=''/>*/}
+
+
+                        {/*</Carousel>*/}
                 </div>
                 <div id="right-div">
                         <h1 id="pageName">GRAPH PATH </h1>
